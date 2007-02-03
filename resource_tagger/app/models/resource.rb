6e121629f17e0af7d531d9ab8498c16be36c9302
Chris Roos <chris@seagul.co.uk>
2007-02-03 07:31:31 +0000

    Remove acts_as_taggable and replace with Resource habtm Tags.  Yay.

--- resource_tagger/app/models/resource.rb
+++ resource_tagger/app/models/resource.rb
@@ -1,3 +1,9 @@
 class Resource < ActiveRecord::Base
-  acts_as_taggable
+  
+  has_and_belongs_to_many :tags
+  
+  def tag(tag_name)
+    tags << Tag.find_or_create_by_name(tag_name)
+  end
+  
 end
--- resource_tagger/test/unit/resource_test.rb
+++ resource_tagger/test/unit/resource_test.rb
@@ -1,10 +1,15 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ResourceTest < Test::Unit::TestCase
-  fixtures :resources
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_should_only_create_one_tag
+    tag = Tag.new(:name => 'TAG')
+    resource_1 = Resource.create!
+    resource_1.tag 'TAG'
+    resource_2 = Resource.create!
+    resource_2.tag 'TAG'
+    
+    assert_equal 2, Tag.find_by_name('TAG').resources.size
   end
+  
 end
