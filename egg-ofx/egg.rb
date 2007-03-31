cbee35c4cb598ba090fd1eeb648add78d9d15f12
Chris Roos <chris@seagul.co.uk>
2007-03-31 16:39:17 +0000

    Move Transaction class to its own file.

--- egg-ofx/egg.rb
+++ egg-ofx/egg.rb
@@ -2,32 +2,7 @@ require File.dirname(__FILE__) + '/date'
 require File.dirname(__FILE__) + '/account'
 require File.dirname(__FILE__) + '/money'
 require File.dirname(__FILE__) + '/statement'
-
-module Egg
-  require 'digest/md5'
-  class Transaction
-    def initialize(date, description, money)
-      @date = Date.build(date)
-      @description = description.squeeze(' ')
-      @amount = Money.new(money).to_f
-      @ofx_id = nil
-    end
-    attr_reader :date, :description, :amount
-    def ofx_id
-      @ofx_id ||= Digest::MD5.hexdigest(to_s)
-    end
-    def to_s
-      [date, description, amount].join(', ')
-    end
-    def type
-      if description =~ /INTEREST/
-        'INT'
-      else
-        (amount < 0) ? 'DEBIT' : 'CREDIT'
-      end
-    end
-  end
-end
+require File.dirname(__FILE__) + '/transaction'
 
 require 'rubygems'
 require 'hpricot'
