1cd6afa47dc1bd2b9add0b39b1b43c23cc284cdd
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2007-03-31 16:38:26 +0000

    Move Statement class to its own file.

--- trunk/egg-ofx/egg.rb
+++ trunk/egg-ofx/egg.rb
@@ -1,29 +1,9 @@
 require File.dirname(__FILE__) + '/date'
 require File.dirname(__FILE__) + '/account'
 require File.dirname(__FILE__) + '/money'
+require File.dirname(__FILE__) + '/statement'
 
 module Egg
-  class Statement
-    def initialize(statement_date, closing_balance, account)
-      from_date, to_date = statement_date.split(' to ')
-      @from_date = Date.build(from_date)
-      @to_date = Date.build(to_date)
-      @closing_balance = Money.new(closing_balance).to_f
-      @account = account
-      @transactions = []
-    end
-    attr_reader :from_date, :to_date, :closing_balance, :transactions
-    def add_transaction(transaction)
-      transaction.date = from_date unless transaction.date
-      @transactions << transaction
-    end
-    def account_currency
-      @account.currency
-    end
-    def account_number
-      @account.number
-    end
-  end
   require 'digest/md5'
   class Transaction
     def initialize(date, description, money)
