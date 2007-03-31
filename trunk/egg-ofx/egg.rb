eb674abd3ba0d755050c9d9efef2804c356e7313
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2007-03-31 16:56:53 +0000

    Iterate over each file in statement directory, creating corresponding ofx file in output directory.

--- trunk/egg-ofx/egg.rb
+++ trunk/egg-ofx/egg.rb
@@ -4,37 +4,43 @@ require File.dirname(__FILE__) + '/lib/ofx'
 require 'rubygems'
 require 'hpricot'
 
-Statement = '2007-03-18'
+StatementsFolder = File.dirname(__FILE__) + '/statements/'
+OfxOutputFolder = File.dirname(__FILE__) + '/ofx/'
 
-html = File.open('statements/' + Statement + '.html') { |f| f.read }
-doc = Hpricot(html)
-
-card_number = (doc/"span#lblCardNumber").inner_html
-statement_date = (doc/"span#lblStatementDate").inner_html
-closing_balance = ((doc/"table#tblTransactionsTable"/"tfoot"/"tr").first/"td").inner_html
-
-account = Egg::Account.new('GBP', card_number)
-statement = Egg::Statement.new(statement_date, closing_balance, account)
-
-(doc/"table#tblTransactionsTable"/"tbody"/"tr").each do |row|
-  date = (row/"td.date").inner_text
-  description = (row/"td.description").inner_text
-  money = (row/"td.money").inner_text
-  next if date == '' || description == 'OPENING BALANCE'
-  if row.next_sibling && (row.next_sibling/"td.date").inner_text == '' && (row.next_sibling/"td.description").inner_text != ''
-    description += ' / ' + (row.next_sibling/"td.description").inner_text
+Dir[StatementsFolder + '*.html'].each do |statement_html_file|
+  
+  html = File.open(statement_html_file) { |f| f.read }
+  doc = Hpricot(html)
+  
+  card_number = (doc/"span#lblCardNumber").inner_html
+  statement_date = (doc/"span#lblStatementDate").inner_html
+  closing_balance = ((doc/"table#tblTransactionsTable"/"tfoot"/"tr").first/"td").inner_html
+  
+  account = Egg::Account.new('GBP', card_number)
+  statement = Egg::Statement.new(statement_date, closing_balance, account)
+  
+  (doc/"table#tblTransactionsTable"/"tbody"/"tr").each do |row|
+    date = (row/"td.date").inner_text
+    description = (row/"td.description").inner_text
+    money = (row/"td.money").inner_text
+    next if date == '' || description == 'OPENING BALANCE'
+    if row.next_sibling && (row.next_sibling/"td.date").inner_text == '' && (row.next_sibling/"td.description").inner_text != ''
+      description += ' / ' + (row.next_sibling/"td.description").inner_text
+    end
+    transaction = Egg::Transaction.new(date, description, money)
+    statement.add_transaction(transaction)
   end
-  transaction = Egg::Transaction.new(date, description, money)
-  statement.add_transaction(transaction)
-end
-
-ofx_s = Ofx::Statement.new(statement)
-
-File.open(File.dirname(__FILE__) + '/ofx/' + Statement + '.ofx', 'w') do |file|
-  file.puts 'OFXHEADER:200'
-  file.puts 'VERSION:203'
-  file.puts 'SECURITY:NONE'
-  file.puts 'OLDFILEUID:NONE'
-  file.puts 'NEWFILEUID:NONE'
-  file.puts(ofx_s.to_xml)
+  
+  ofx_s = Ofx::Statement.new(statement)
+  
+  filename = File.basename(statement_html_file, '.html')
+  File.open(OfxOutputFolder + filename + '.ofx', 'w') do |file|
+    file.puts 'OFXHEADER:200'
+    file.puts 'VERSION:203'
+    file.puts 'SECURITY:NONE'
+    file.puts 'OLDFILEUID:NONE'
+    file.puts 'NEWFILEUID:NONE'
+    file.puts(ofx_s.to_xml)
+  end
+  
 end
