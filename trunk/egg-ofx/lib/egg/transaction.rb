edcb990cc7c8aaa8859bba4f5d9e2e1abb29e314
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2007-04-01 12:30:49 +0000

    Output the payee and memo in Ofx in the hope that wesabe will better match existing rules with these transactions.

--- trunk/egg-ofx/lib/egg/transaction.rb
+++ trunk/egg-ofx/lib/egg/transaction.rb
@@ -1,22 +1,30 @@
 require 'digest/md5'
+require File.expand_path(File.dirname(__FILE__) + '/description')
 
 module Egg
   class Transaction
     def initialize(date, description, money)
       @date = Date.build(date)
-      @description = description.squeeze(' ')
+      @raw_description = description
+      @description = Description.new(description)
       @amount = Money.new(money).to_f
       @ofx_id = nil
     end
-    attr_reader :date, :description, :amount
+    attr_reader :date, :amount
+    def payee
+      @description.payee
+    end
+    def note
+      @description.note
+    end
     def ofx_id
       @ofx_id ||= Digest::MD5.hexdigest(to_s)
     end
     def to_s
-      [date, description, amount].join(', ')
+      [date, @raw_description, amount].join(', ')
     end
     def type
-      if description =~ /INTEREST/
+      if payee =~ /INTEREST/
         'INT'
       else
         (amount < 0) ? 'DEBIT' : 'CREDIT'
