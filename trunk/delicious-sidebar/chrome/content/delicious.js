550b28ce772098c764980a8d602f904d6e992343
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2007-10-18 16:49:21 +0000

    Load my delicious sidebar page rather than delicious itself.

--- trunk/delicious-sidebar/chrome/content/delicious.js
+++ trunk/delicious-sidebar/chrome/content/delicious.js
@@ -1,6 +1,6 @@
 function openDeliciousUrl(url) {
   var browser = document.getElementById('sidebar').contentDocument.getElementById('browserid');
   var urlHash = hex_md5(url);
-  var deliciousUrl = 'http://del.icio.us/url/' + urlHash
+  var deliciousUrl = 'http://seagul.co.uk/delicious/' + urlHash
   browser.webNavigation.loadURI(deliciousUrl, Components.interfaces.nsIWebNavigation.LOAD_FLAGS_NONE, null, null, null);
 };
