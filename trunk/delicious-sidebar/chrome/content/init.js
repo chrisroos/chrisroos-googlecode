c648dfb848237a63187d0c5ff25a980c0f554234
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2007-11-01 22:13:57 +0000

    The previous implementation was responding to events fired whenever any content was loaded, including in iframes for example.  This new implementation has been shamelessly stolen from http://developer.mozilla.org/en/docs/Code_snippets:Progress_Listeners

--- trunk/delicious-sidebar/chrome/content/init.js
+++ trunk/delicious-sidebar/chrome/content/init.js
@@ -1,15 +1,47 @@
-window.addEventListener("load", function() { myExtension.init(); }, false);
-
-var myExtension = {
-  init: function() {
-    var appcontent = document.getElementById("appcontent");   // browser
-    if(appcontent)
-      appcontent.addEventListener("DOMContentLoaded", this.onPageLoad, true);
+var myExt_urlBarListener = {
+  QueryInterface: function(aIID)
+  {
+   if (aIID.equals(Components.interfaces.nsIWebProgressListener) ||
+       aIID.equals(Components.interfaces.nsISupportsWeakReference) ||
+       aIID.equals(Components.interfaces.nsISupports))
+     return this;
+   throw Components.results.NS_NOINTERFACE;
   },
 
-  onPageLoad: function(aEvent) {
-    var doc = aEvent.originalTarget; // doc is document that triggered "onload" event
-    var url = doc.location.href;
-    openDeliciousUrl(url);
+  onLocationChange: function(aProgress, aRequest, aURI)
+  {
+    myExtension.processNewURL(aURI);
+  },
+
+  onStateChange: function() {},
+  onProgressChange: function() {},
+  onStatusChange: function() {},
+  onSecurityChange: function() {},
+  onLinkIconAvailable: function() {}
+};
+
+var myExtension = {
+  oldURL: null,
+  
+  init: function() {
+    // Listen for webpage loads
+    gBrowser.addProgressListener(myExt_urlBarListener,
+        Components.interfaces.nsIWebProgress.NOTIFY_STATE_DOCUMENT);
+  },
+  
+  uninit: function() {
+    gBrowser.removeProgressListener(myExt_urlBarListener);
+  },
+
+  processNewURL: function(aURI) {
+    if (aURI.spec == this.oldURL)
+      return;
+    
+    // now we know the url is new...
+    openDeliciousUrl(aURI.spec);
+    this.oldURL = aURI.spec;
   }
-}
+};
+
+window.addEventListener("load", function() {myExtension.init()}, false);
+window.addEventListener("unload", function() {myExtension.uninit()}, false);
