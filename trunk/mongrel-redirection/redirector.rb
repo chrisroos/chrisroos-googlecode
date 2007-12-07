fb16efc702e060a53ac5de0845276c8b806a587e
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2007-12-07 06:26:21 +0000

    I think it makes the code slightly more understandable when we only have one set of rules, rather than splitting them into host redirections and host _and_ path redirections.  This should also help when we want to load rules in from a configuration file.

--- trunk/mongrel-redirection/redirector.rb
+++ trunk/mongrel-redirection/redirector.rb
@@ -1,19 +1,18 @@
 class Redirector
   
-  def initialize(request, host_redirection_rules = {}, redirection_rules = {})
+  def initialize(request, redirection_rules = {})
     @path = request.params['REQUEST_PATH']
     @host = request.params['HTTP_X_FORWARDED_HOST']
     @uri = request.params['REQUEST_URI']
-    @host_redirection_rules = host_redirection_rules
     @redirection_rules = redirection_rules
   end
   
   def redirect_to
-    redirect_to = @host_redirection_rules[@host]
-    return "http://#{redirect_to}#{@uri}" if redirect_to
-    
     redirect_to = @redirection_rules[@host][@path] rescue nil
     return redirect_to if redirect_to
+    
+    redirect_to = @redirection_rules[@host]
+    return "http://#{redirect_to}#{@uri}" if redirect_to && redirect_to.is_a?(String)
     
     unless @path == '/'
       path_without_trailing_slash = @path.gsub(/\/$/, '')
