646cd63cdcbb6bb8af8dbe067244c1ba49971273
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2007-12-18 21:34:39 +0000

    An extension of an extension.  Yay.  Instead of amending the actual extension we can use this to build on what's already been added.

---
+++ trunk/delicious-permalinks/chrome/content/deliciousPermalinks.js
@@ -0,0 +1,18 @@
+var deliciousPermalinks = {
+  addPermalinkTag : function() {
+    var post = window.arguments[0];
+    var urlTag = 'url/' + hex_md5(post.url);
+    if (!this.tagExists(urlTag, post.tags)) {
+      post.tags.push(urlTag);
+      yAddBookMark._addToUserTags(urlTag);
+    }
+  },
+  tagExists : function(tag, tags) {
+    for (var i = 0; i < tags.length; ++i) {
+      if (tag == tags[i]) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
