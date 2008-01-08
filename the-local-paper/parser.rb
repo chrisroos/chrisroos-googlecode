7b458cf409afa0bfdc4d05d42df86356665d02d9
Chris Roos <chris@seagul.co.uk>
2008-01-08 09:35:57 +0000

    We are now able to take a loosely structured csv file of article data and turn it into a bunch of static article pages and an index.

---
+++ the-local-paper/parser.rb
@@ -0,0 +1,37 @@
+require 'csv'
+
+class PaperParser
+  def self.parse(filename)
+    new(filename).parse
+  end
+  def initialize(filename)
+    filename =~ /^(\d{4}-\d{2}-\d{2})-(.*)\.txt$/
+    @date, @title = $1, $2
+  end
+  def parse
+    {
+      :date => Date.parse(@date),
+      :title => @title.gsub(/-/, ' ')
+    }
+  end
+end
+
+class ArticleParser
+  def initialize(io)
+    @io = io
+  end
+  def each
+    @io.each do |line|
+      next if line =~ /^#/
+      page_number, title, author, *attributes = CSV.parse_line(line)
+      attributes = attributes.inject({}) { |h, key_value| key, value = key_value.split(':'); h[key] = value; h }
+      data = {
+        :page_number => page_number,
+        :title => title,
+        :author => author,
+        :attributes => attributes
+      }
+      yield data
+    end
+  end
+end
