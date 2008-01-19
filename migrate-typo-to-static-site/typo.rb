083429da40b8e3844c3f6902677d7bd0d6a233b7
Chris Roos <chris@seagul.co.uk>
2008-01-19 15:36:12 +0000

    I think the creation of article pages is basically complete.  Woohoo.

--- migrate-typo-to-static-site/typo.rb
+++ migrate-typo-to-static-site/typo.rb
@@ -18,7 +18,81 @@ ActiveRecord::Base.establish_connection({
   :user => 'root'
 })
 
-class Article < ActiveRecord::Base; end
-class Trackback < ActiveRecord::Base; end
-class Comment < ActiveRecord::Base; end
+require 'syntax/convertors/html'
+
+class SyntaxHighlighter
+  def self.markup_code(code = '', lang = 'default')
+    cssclass = nil
+    linenumber = nil
+    title = nil
+
+    code = code.to_s.gsub(/\r/,'').gsub(/\A\n/,'').chomp
+
+    convertor = Syntax::Convertors::HTML.for_syntax lang
+    code = convertor.convert(code)
+    code.gsub!(/<pre>/,"<pre><code class=\"typocode_#{lang} #{cssclass}\"><notextile>")
+    code.gsub!(/<\/pre>/,"</notextile></code></pre>")
+
+    if(linenumber)
+      lines = code.split(/\n/).size
+      linenumbers = (1..lines).to_a.collect{|line| line.to_s}.join("\n")
+
+      code = "<table class=\"typocode_linenumber\"><tr><td class=\"lineno\">\n<pre>\n#{linenumbers}\n</pre>\n</td><td width=\"100%\">#{code}</td></tr></table>"
+    end
+
+    if(title)
+      titlecode="<div class=\"codetitle\">#{title}</div>"
+    else
+      titlecode=''
+    end
+
+    "<div class=\"typocode\">#{titlecode}#{code}</div>"
+  end
+end
+
+require 'redcloth'
+
+class Article < ActiveRecord::Base
+  has_many :comments, :dependent => :destroy, :order => 'created_at ASC', :conditions => 'published = TRUE'
+  has_many :trackbacks, :dependent => :destroy, :order => 'created_at ASC'
+  has_and_belongs_to_many :tags, :foreign_key => 'article_id'
+  def formatted_published_date
+    published_at.strftime("%a, %d %b %Y %H:%M:%S")
+  end
+  def body_html
+    body = self.body.gsub(/<typo:code(.*?)>(.*?)<\/typo:code>/m) do
+      code = $2
+      lang = $1[/lang="(.*?)"/, 1]
+      SyntaxHighlighter.markup_code(code, lang)
+    end
+    RedCloth.new(body).to_html(:textile)
+  end
+  public :binding
+end
+class Tag < ActiveRecord::Base
+  has_and_belongs_to_many :articles, :order => 'created_at DESC'
+end
+class Trackback < ActiveRecord::Base
+  belongs_to :article
+end
+class Comment < ActiveRecord::Base
+  belongs_to :article
+  def body_html
+    RedCloth.new(body).to_html(:textile)
+  end
+  def formatted_created_date
+    created_at.strftime("%a, %d %b %Y %H:%M:%S")
+  end
+end
 class Page < ActiveRecord::Base; end
+
+article = Article.find_by_permalink('textmate-command-to-display-active-record-column-attributes')
+
+require 'erb'
+include ERB::Util
+
+article_template = File.open('article.erb.html') { |f| f.read }
+article_erb = ERB.new(article_template)
+File.open("Site/articles/#{article.permalink}.html", 'w') do |file|
+  file.puts article_erb.result(article.binding)
+end
