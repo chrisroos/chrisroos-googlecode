f90a358807aa1b2731960570f0f7e53112f085ff
Chris Roos <chris@seagul.co.uk>
2008-01-20 17:45:56 +0000

    Yahoo somehow has a bunch of urls in its index that have the text 'tags' in them.  Google doesn't have these so I wonder if an older version of typo used tags inplace of tag and at that time we were only being indexed by yahoo.  A request for one of 'tags' urls against by current typo deployment returns a 200 response which is why I guess they haven't dropped out of the index.  I'm going to redirect them to the corresponding 'tag' version and assume that if the tag genuinely doesn't exist that a 404 should know the page from the index.

--- migrate-typo-to-static-site/search-engine-indexes/yahoo_parser.rb
+++ migrate-typo-to-static-site/search-engine-indexes/yahoo_parser.rb
@@ -7,6 +7,7 @@ File.open('blog.seagul.co.uk_all_pages.tsv') do |file|
 end
 
 # blog_urls_in_index = ['http://blog1.seagul.co.uk/articles/tag/ruby/page/2']
+# blog_urls_in_index = ['http://blog1.seagul.co.uk/articles/tags/irb']
 
 require 'net/http'
 
@@ -22,14 +23,21 @@ blog_urls_in_index.compact.sort.each do |url|
   if response.code == '200'
     # All fine - don't report anything
   elsif response.code == '301'
-    if request_url =~ /(articles\/tag\/.+?)\/page/
-      # A request for a paged tag resource.  Should be redirected to the resource with the paging part.
-      expected_redirection = "http://blog1.seagul.co.uk/#{$1}"
+    if request_url =~ /articles\/tag\/(.+?)\/page/
+      # A request for a paged tag resource.  Should be redirected to the resource with the paging part removed.
+      expected_redirection = "http://blog1.seagul.co.uk/articles/tag/#{$1}"
+      unless expected_redirection == response['Location']
+        print "Requested: #{request_url}  ...  incorrectly redirected (301) to #{response['Location']}"
+      end
+    elsif request_url =~ /articles\/tags\/(.+)/
+      # A request for an old tags resource.  Should be redirected to the corresponding tag resource.
+      expected_redirection = "http://blog1.seagul.co.uk/articles/tag/#{$1}"
       unless expected_redirection == response['Location']
         print "Requested: #{request_url}  ...  incorrectly redirected (301) to #{response['Location']}"
       end
     else
       print "Requested: #{request_url}  ...  "
+      puts "Redirected to #{response['Location']}"
     end
   else
     print "Requested: #{request_url}  ...  "
