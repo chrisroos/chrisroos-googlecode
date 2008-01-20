c79729d5493aa3ac768208969e4f633bdd0b6c31
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-01-20 14:07:54 +0000

    Simplify the syntax highlighting code.

--- trunk/migrate-typo-to-static-site/site-generation/syntax_highlighter.rb
+++ trunk/migrate-typo-to-static-site/site-generation/syntax_highlighter.rb
@@ -2,30 +2,13 @@ require 'syntax/convertors/html'
 
 class SyntaxHighlighter
   def self.markup_code(code = '', lang = 'default')
-    cssclass = nil
-    linenumber = nil
-    title = nil
-
     code = code.to_s.gsub(/\r/,'').gsub(/\A\n/,'').chomp
 
     convertor = Syntax::Convertors::HTML.for_syntax lang
     code = convertor.convert(code)
-    code.gsub!(/<pre>/,"<pre><code class=\"typocode_#{lang} #{cssclass}\"><notextile>")
+    code.gsub!(/<pre>/,"<pre><code class=\"typocode_#{lang}\"><notextile>")
     code.gsub!(/<\/pre>/,"</notextile></code></pre>")
 
-    if(linenumber)
-      lines = code.split(/\n/).size
-      linenumbers = (1..lines).to_a.collect{|line| line.to_s}.join("\n")
-
-      code = "<table class=\"typocode_linenumber\"><tr><td class=\"lineno\">\n<pre>\n#{linenumbers}\n</pre>\n</td><td width=\"100%\">#{code}</td></tr></table>"
-    end
-
-    if(title)
-      titlecode="<div class=\"codetitle\">#{title}</div>"
-    else
-      titlecode=''
-    end
-
-    "<div class=\"typocode\">#{titlecode}#{code}</div>"
+    "<div class=\"typocode\">#{code}</div>"
   end
 end
