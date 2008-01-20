518c234b0ebe6b9c4f29ec4f9f67888e87f2c6f1
Chris Roos <chris@seagul.co.uk>
2008-01-20 16:06:42 +0000

    Move the PageGenerator into its own file.

--- migrate-typo-to-static-site/site-generation/typo.rb
+++ migrate-typo-to-static-site/site-generation/typo.rb
@@ -12,51 +12,13 @@
 require File.join(File.dirname(__FILE__), 'environment')
 require File.join(MIGRATOR_ROOT, 'article')
 require File.join(MIGRATOR_ROOT, 'page')
-require File.join(MIGRATOR_ROOT, 'erb_renderer')
+require File.join(MIGRATOR_ROOT, 'page_generator')
 
-class PageGenerator
-  
-  # Previously we were sending the binding of an article, tag, or page to their respective template.
-  # The erb templates still expect to be able to access these variables.
-  # This template object allows us to magically define methods on it.  So, for example, we can define an article
-  # method that returns an instance of an article.  By instantiating a template for each object in our collection
-  # and setting the variable required by the erb template we can remove some duplication.
-  # Example.
-  # Instead of rendering the article erb template in the context of an article (article.binding)
-  # We instantiate a template passing it :article and an article instance.  This gives us an article method on the template
-  # that returns our article instance.  We can then render the article erb template in the context of this template
-  # object.
-  class Template
-    def metaclass
-      class << self; self; end
-    end
-    def initialize(object_name, object)
-      metaclass.__send__(:define_method, object_name) { object }
-    end
-    public :binding
-  end
-      
-  def initialize(collection, object_name)
-    @collection, @object_name = collection, object_name
-  end
-  
-  def generate
-    @collection.each do |object|
-      FileUtils.mkdir_p(File.join(SITE_ROOT, object.path))
+find_options = {:limit => 1}
 
-      erb_template = File.join(TEMPLATE_ROOT, "#{@object_name}.erb.html")
-      template = Template.new(@object_name, object)
-      
-      renderer = ErbRenderer.new(erb_template, template.binding)
-      File.open(File.join(SITE_ROOT, "#{object.url}.html"), 'w') { |io| renderer.render(io) }
-    end
-  end
-  
-end
-
-articles = Article.find(:all)
-tags = Tag.find(:all)
-pages = Page.find(:all)
+articles = Article.find(:all, find_options)
+tags = Tag.find(:all, find_options)
+pages = Page.find(:all, find_options)
 
 PageGenerator.new(articles, :article).generate
 PageGenerator.new(tags, :tag).generate
