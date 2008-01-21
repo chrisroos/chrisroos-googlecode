34fe19cac753659005bc914ec00e1d29fdb9fa1c
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-01-21 22:47:06 +0000

    Tightern up our pattern matching by ensuring that articles or pages are at the root of the url.

--- trunk/migrate-typo-to-static-site/search-engine-indexes/yahoo_parser.rb
+++ trunk/migrate-typo-to-static-site/search-engine-indexes/yahoo_parser.rb
@@ -20,98 +20,98 @@ rules = [
   },
   {
     :description => "The articles resource should be redirected to the root resource (they are, afterall, the same thing).",
-    :pattern => /articles\/$/,
-    :expected_location => proc {"http://blog1.seagul.co.uk/"},
+    :pattern => /^\/articles\/$/,
+    :expected_location => proc { "http://blog1.seagul.co.uk/" },
     :expected_code => '301'
   },
   {
     :description => "Paged tag resources should be redirected to the non-paged tag resource.",
-    :pattern => /articles\/tag\/(.+?)\/page/,
-    :expected_location => proc {"http://blog1.seagul.co.uk/articles/tag/#{$1}"},
+    :pattern => /^\/articles\/tag\/(.+?)\/page/,
+    :expected_location => proc { "http://blog1.seagul.co.uk/articles/tag/#{$1}" },
     :expected_code => '301'
   },
   {
     :description => "Old tag resources (/tags instead of /tag) should be redirected to the correct tag resource.",
-    :pattern => /articles\/tags\/(.+)/,
-    :expected_location => proc {"http://blog1.seagul.co.uk/articles/tag/#{$1}"},
+    :pattern => /^\/articles\/tags\/(.+)/,
+    :expected_location => proc { "http://blog1.seagul.co.uk/articles/tag/#{$1}" },
     :expected_code => '301'
   },
   {
     :description => "Paged year resources should be redirected to the non-paged year resource.",
-    :pattern => /articles\/(\d{4})\/page.*/,
-    :expected_location => proc {"http://blog1.seagul.co.uk/articles/#{$1}"},
+    :pattern => /^\/articles\/(\d{4})\/page.*/,
+    :expected_location => proc { "http://blog1.seagul.co.uk/articles/#{$1}" },
     :expected_code => '301'
   },
   {
     :description => "Paged year/month resources should be redirected to the non-paged year/month resource.",
-    :pattern => /articles\/(\d{4})\/(\d{2})\/page.*/,
-    :expected_location => proc {"http://blog1.seagul.co.uk/articles/#{$1}/#{$2}"},
+    :pattern => /^\/articles\/(\d{4})\/(\d{2})\/page.*/,
+    :expected_location => proc { "http://blog1.seagul.co.uk/articles/#{$1}/#{$2}" },
     :expected_code => '301'
   },
   {
     :description => "Year/month/day/index.html resource should be redirected to the root year/month/day/ resource.",
-    :pattern => /articles\/(\d{4})\/(\d{2})\/(\d{2})\/index\.html/,
-    :expected_location => proc {"http://blog1.seagul.co.uk/articles/#{$1}/#{$2}/#{$3}/"},
+    :pattern => /^\/articles\/(\d{4})\/(\d{2})\/(\d{2})\/index\.html/,
+    :expected_location => proc { "http://blog1.seagul.co.uk/articles/#{$1}/#{$2}/#{$3}/" },
     :expected_code => '301'
   },
   {
     :description => "Paged year/month/day resources should be redirected to the non-paged year/month/day resource.",
-    :pattern => /articles\/(\d{4})\/(\d{2})\/(\d{2})\/page.*/,
-    :expected_location => proc {"http://blog1.seagul.co.uk/articles/#{$1}/#{$2}/#{$3}"},
+    :pattern => /^\/articles\/(\d{4})\/(\d{2})\/(\d{2})\/page.*/,
+    :expected_location => proc { "http://blog1.seagul.co.uk/articles/#{$1}/#{$2}/#{$3}" },
     :expected_code => '301'
   },
   {
     :description => "Tag resources should render OK",
-    :pattern => /articles\/tag\/.*/,
+    :pattern => /^\/articles\/tag\/.*/,
     :expected_code => '200'
   },
   {
     :description => "Page resources should render OK",
-    :pattern => /\/pages\/.+/,
+    :pattern => /^\/pages\/.+/,
     :expected_code => '200'
   },
   {
     :description => "Year resources without a trailing slash should redirect to the same year resource with a trailing slash",
-    :pattern => /articles\/(\d{4})$/,
-    :expected_location => proc {"http://blog1.seagul.co.uk/articles/#{$1}/"},
+    :pattern => /^\/articles\/(\d{4})$/,
+    :expected_location => proc { "http://blog1.seagul.co.uk/articles/#{$1}/" },
     :expected_code => '301'
   },
   {
     :description => "Year resources should render OK",
-    :pattern => /articles\/\d{4}\/$/,
+    :pattern => /^\/articles\/\d{4}\/$/,
     :expected_code => '200'
   },
   {
     :description => "Year/month resources without a trailing slash should redirect to the same resource with the trailing slash",
-    :pattern => /articles\/(\d{4})\/(\d{2})$/,
-    :expected_location => proc { "http://blog1.seagul.co.uk/articles/#{$1}/#{$2}/"},
+    :pattern => /^\/articles\/(\d{4})\/(\d{2})$/,
+    :expected_location => proc { "http://blog1.seagul.co.uk/articles/#{$1}/#{$2}/" },
     :expected_code => '301'
   },
   {
     :description => "Year/month resources with a one digit month should redirect to the same resource with a two digit month",
-    :pattern => /articles\/(\d{4})\/(\d{1})$/,
-    :expected_location => proc { "http://blog1.seagul.co.uk/articles/#{$1}/0#{$2}"},
+    :pattern => /^\/articles\/(\d{4})\/(\d{1})$/,
+    :expected_location => proc { "http://blog1.seagul.co.uk/articles/#{$1}/0#{$2}" },
     :expected_code => '301'
   },
   {
     :description => "Paged year/one-digit-month resources should be redirected to the non-paged year/two-digit-month resource.",
-    :pattern => /articles\/(\d{4})\/(\d{1})\/page.*/,
+    :pattern => /^\/articles\/(\d{4})\/(\d{1})\/page.*/,
     :expected_location => proc {"http://blog1.seagul.co.uk/articles/#{$1}/0#{$2}"},
     :expected_code => '301'
   },
   {
     :description => "Year/month resources should render OK",
-    :pattern => /articles\/\d{4}\/\d{2}\/$/,
+    :pattern => /^\/articles\/\d{4}\/\d{2}\/$/,
     :expected_code => '200'
   },
   {
     :description => "Year/month/day resources should render OK",
-    :pattern => /articles\/\d{4}\/\d{2}\/\d{2}\/$/,
+    :pattern => /^\/articles\/\d{4}\/\d{2}\/\d{2}\/$/,
     :expected_code => '200'
   },
   {
     :description => "Article resources should render OK",
-    :pattern => /articles\/\d{4}\/\d{2}\/\d{2}\/.+$/,
+    :pattern => /^\/articles\/\d{4}\/\d{2}\/\d{2}\/.+$/,
     :expected_code => '200'
   },
   {
