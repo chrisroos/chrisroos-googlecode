71bd36bc9f4d0af751378c7deac268667a547b49
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-01-23 08:56:47 +0000

    When I came to generate the rss xml feed I realised that the page generator needed to be a little more liberal in what it named its files (i.e. it should default, but shouldn't be restricted to, .html)

--- trunk/migrate-typo-to-static-site/site-generation/lib/page_generator.rb
+++ trunk/migrate-typo-to-static-site/site-generation/lib/page_generator.rb
@@ -2,16 +2,16 @@ require 'erb_renderer'
 
 class PageGenerator
       
-  def initialize(view, template)
-    @view, @template = view, template
+  def initialize(view, template, extension = 'html')
+    @view, @template, @extension = view, template, extension
   end
   
   def generate
     FileUtils.mkdir_p(File.join(SITE_ROOT, @view.path))
 
-    erb_template = File.join(TEMPLATE_ROOT, "#{@template}.erb.html")
+    erb_template = File.join(TEMPLATE_ROOT, "#{@template}.erb.#{@extension}")
     renderer = ErbRenderer.new(erb_template, @view.binding)
-    File.open(File.join(SITE_ROOT, "#{@view.url}.html"), 'w') { |io| renderer.render(io) }
+    File.open(File.join(SITE_ROOT, "#{@view.url}.#{@extension}"), 'w') { |io| renderer.render(io) }
   end
   
 end
