f06fc2c9a4222d456f436c183db372d48e8f3d85
Chris Roos <chris@seagul.co.uk>
2008-01-24 08:29:20 +0000

    By replacing 'created_at' with 'published_at' when ordering all articles for a tag I can remove the need for the Article#created_at column altogether.  Article#updated_at is not used either.

--- migrate-typo-to-static-site/site-generation/models/tag.rb
+++ migrate-typo-to-static-site/site-generation/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ActiveRecord::Base
   
-  has_and_belongs_to_many :articles, :order => 'created_at DESC'
+  has_and_belongs_to_many :articles, :order => 'published_at DESC'
   
   def path
     TAGS_URL_ROOT
