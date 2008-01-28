f59c171a1715dae1d60c3aa73be41f8e1489e48b
Chris Roos <chris@seagul.co.uk>
2008-01-28 09:51:43 +0000

    It's fairly obvious that I copied this from something I did at work...

--- migrate-typo-to-static-site/db/migrate.rb
+++ migrate-typo-to-static-site/db/migrate.rb
@@ -4,9 +4,9 @@ require 'yaml'
 warn "Make sure you update schema_info to 0, or drop the table, before running these migrations for the first time.  Otherwise, they just won't work..."
 
 database_config = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'config', 'database.yml'))
-reevoo_slave_config = database_config['production']
+database_config = database_config['production']
 
 ActiveRecord::Base.logger = Logger.new(StringIO.new)
-ActiveRecord::Base.establish_connection(reevoo_slave_config)
+ActiveRecord::Base.establish_connection(database_config)
 
 ActiveRecord::Migrator.migrate(File.join(File.dirname(__FILE__), 'migrate'))
