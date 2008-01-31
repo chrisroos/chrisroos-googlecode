f95393f2b3581d5984c80af4adcb2aa89f2e951d
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-01-31 13:11:13 +0000

    No need to repeat ourselves any longer.

--- trunk/migrate-typo-to-static-site/db/migrate.rb
+++ trunk/migrate-typo-to-static-site/db/migrate.rb
@@ -1,12 +1,7 @@
+require File.join(File.dirname(__FILE__), '..', 'config', 'environment')
 require 'active_record'
 require 'yaml'
 
 warn "Make sure you update schema_info to 0, or drop the table, before running these migrations for the first time.  Otherwise, they just won't work..."
-
-database_config = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'config', 'database.yml'))
-database_config = database_config['production']
-
-ActiveRecord::Base.logger = Logger.new(StringIO.new)
-ActiveRecord::Base.establish_connection(database_config)
 
 ActiveRecord::Migrator.migrate(File.join(File.dirname(__FILE__), 'migrate'))
