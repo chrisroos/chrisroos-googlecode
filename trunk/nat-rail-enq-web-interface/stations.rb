a7de04087c54d24198e25ebce316199ac1c5893f
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-01-31 14:35:55 +0000

    A little tidying up of the parsing code.

--- trunk/nat-rail-enq-web-interface/stations.rb
+++ trunk/nat-rail-enq-web-interface/stations.rb
@@ -1,11 +1,31 @@
 require 'json'
 
-station_search_term = 'durham'
+class StationsParser
+  def self.from_file(stations_as_json_path)
+    stations_as_json = File.open(stations_as_json_path) { |file| file.read }
+    JSON.parse(stations_as_json)
+  end
+end
 
-stations_json = File.open("/Users/chrisroos/Desktop/stations.js") { |file| file.read }
-station_codes_and_stations = JSON.parse(stations_json)
-STATIONS_BY_CODE = station_codes_and_stations.inject({}) { |hash, station_code_and_station| hash[station_code_and_station.keys.first] = station_code_and_station.values.first; hash }
-CODES_BY_STATION = station_codes_and_stations.inject({}) { |hash, station_code_and_station| hash[station_code_and_station.values.first] = station_code_and_station.keys.first; hash }
+class Stations
+  def initialize(station_codes_and_stations)
+    @stations = station_codes_and_stations.inject({}) do |hash, station_code_and_station|
+      hash[station_code_and_station.values.first] = station_code_and_station.keys.first
+      hash
+    end
+  end
+  def [](code)
+    @stations[code]
+  end
+end
 
-# p STATIONS_BY_CODE.keys.select { |station_code| station_code =~ /ram/i }
-CODES_BY_STATION.keys.select { |station| station =~ /#{station_search_term}/i }.sort.each { |station| p [station, CODES_BY_STATION[station]] }
+stations_json_path = File.dirname(__FILE__) + '/stations.js'
+station_codes_and_stations = StationsParser.from_file(stations_json_path)
+
+
+# STATIONS_BY_CODE = station_codes_and_stations.inject({}) { |hash, station_code_and_station| hash[station_code_and_station.keys.first] = station_code_and_station.values.first; hash }
+# p STATIONS_BY_CODE
+# STATION_CODES = 
+# STATIONS = STATION_CODES.keys
+# 
+# # CODES_BY_STATION.keys.select { |station| station =~ /#{station_search_term}/i }.sort.each { |station| p [station, CODES_BY_STATION[station]] }
