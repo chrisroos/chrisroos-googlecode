d572ccfccddf594893c86ff9ecc7a8900ee78ebb
Chris Roos <chris@seagul.co.uk>
2008-02-08 08:13:42 +0000

    Split the models into their own files to make testing slightly easier.

--- the-local-paper/generate-pages.rb
+++ the-local-paper/generate-pages.rb
@@ -1,64 +1,8 @@
-class Date
-  def iso8601
-    Time.parse(self.to_s).iso8601
-  end
-end
-
-class Paper
-  public :binding
-  attr_reader :title, :date, :articles
-  def initialize(title, date)
-    @title, @date = title, date
-    @articles, @articles_by_page_number = [], {}
-  end
-  def add_article(article)
-    @articles << article
-    @articles_by_page_number[article.page_number] ||= []
-    @articles_by_page_number[article.page_number] << article
-  end
-  def each_page_with_articles
-    @articles_by_page_number.sort { |(p1, a1), (p2, a2)| p1 <=> p2 }.each { |page_number, articles| yield page_number, articles }
-  end
-  def html_title
-    "#{self.title} on #{self.human_friendly_date}"
-  end
-  def human_friendly_date
-    self.date.strftime("%A %d %b %Y")
-  end
-  def iso8601_date
-    self.date.iso8601
-  end
-  def url_friendly_title
-    title.downcase.gsub(/ /, '-')
-  end
-end
-
-class Article
-  public :binding
-  attr_reader :paper, :title, :author, :page_number, :other_attributes
-  def initialize(paper, title, author, page_number, other_attributes)
-    @paper, @title, @author, @page_number, @other_attributes = paper, title, author, Integer(page_number), other_attributes
-    @paper.add_article(self)
-  end
-  def html_title
-    "#{self.title} - #{paper.html_title}"
-  end
-  def iso8601_published_date
-    paper.iso8601_date
-  end
-  def human_friendly_published_date
-    paper.human_friendly_date
-  end
-  def paper_name
-    paper.title
-  end
-  def url_friendly_title
-    title.downcase.gsub(/ /, '-').gsub(/[^a-zA-Z0-9-]/, '').squeeze('-')
-  end  
-end
-
 # Let's use the parser to extract some real data and construct our object graph
 require File.join(File.dirname(__FILE__), 'parser')
+require File.join(File.dirname(__FILE__), 'article')
+require File.join(File.dirname(__FILE__), 'paper')
+require File.join(File.dirname(__FILE__), 'date')
 
 SITE_FOLDER = File.join(File.dirname(__FILE__), 'Site')
 RAW_DATA_FOLDER = File.join(SITE_FOLDER, 'raw-data')
