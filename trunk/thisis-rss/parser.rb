c61737c6ecc0afb5b868d86e7f65861a9303a2b0
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-02-13 14:18:07 +0000

    Two simple comments.

--- trunk/thisis-rss/parser.rb
+++ trunk/thisis-rss/parser.rb
@@ -15,6 +15,10 @@ parser.each_article_attributes do |article_attributes|
   articles << article_attributes
 end
 
+# STORE THE ARTICLES IN A 'DATABASE' CHECKING FOR DUPLICATES ALONG THE WAY
+
+# RENDER THE NEWS ATOM FEED FROM THE ARTICLES IN THE 'DATABASE'
+
 erb_template = File.join(File.dirname(__FILE__), "news.erb.atom")
 renderer = ErbRenderer.new(erb_template, binding)
 File.open(File.join(File.dirname(__FILE__), "news.atom"), 'w') { |io| renderer.render(io) }
