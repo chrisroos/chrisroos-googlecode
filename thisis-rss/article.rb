38432f860cd332771813069557cec7b0e90c72df
Chris Roos <chris@seagul.co.uk>
2008-02-13 14:09:34 +0000

    I've moved away from the idea of an actual article object (for now) - hashes are just a little easier.

--- thisis-rss/article.rb
+++
@@ -1,6 +0,0 @@
-class Article
-  attr_reader :id, :date, :headline, :content, :url
-  def initialize(id, date, headline, content, url)
-    @id, @date, @headline, @content, @url = id, date, headline, content, url
-  end
-end
