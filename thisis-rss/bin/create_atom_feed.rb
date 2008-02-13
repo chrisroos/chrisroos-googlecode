bd30e9d7fcbf5de81ea3de310effe5e6bab7f925
Chris Roos <chris@seagul.co.uk>
2008-02-13 22:35:26 +0000

    Move the parser (well, it used to be a parser playground, now it kinda glues everything together to allow us to produce an atom feed from some html input.

--- thisis-rss/bin/create_atom_feed.rb
+++ thisis-rss/bin/create_atom_feed.rb
@@ -1,20 +1,24 @@
-$: << File.join(File.dirname(__FILE__), 'lib')
+$: << File.join(File.dirname(__FILE__), '..', 'lib')
 require 'erb_renderer'
 require 'document_parser'
 require 'articles'
 require 'articles_view'
+
+DATA_DIRECTORY = File.dirname(__FILE__)
+HTML_DIRECTORY = File.join(File.dirname(__FILE__), '..')
+TEMPLATE_DIRECTORY = File.join(File.dirname(__FILE__), '..')
 
 # download articles
 # compare articles to those currently stored in the 'database'
 # add to the database if not found
 # reproduce feed from database
 
-ArticlesFilename = File.join(File.dirname(__FILE__), 'articles.yaml')
+ArticlesFilename = File.join(DATA_DIRECTORY, 'articles.yaml')
 File.open(ArticlesFilename, 'w') unless File.exists?(ArticlesFilename) # Create if it doesn't already exist
 
 articles = Articles.retrieve_from_yaml_file(ArticlesFilename)
 
-html_filename = File.join(File.dirname(__FILE__), 'sample-news.html')
+html_filename = File.join(HTML_DIRECTORY, 'sample-news.html')
 
 parser = DocumentParser.from_file(html_filename)
 parser.each_article_attributes do |article_attributes|
@@ -25,6 +29,6 @@ articles.store_to_yaml_file(ArticlesFilename)
 
 articles_view = ArticlesView.new(articles, 2)
 
-erb_template = File.join(File.dirname(__FILE__), "news.erb.atom")
+erb_template = File.join(TEMPLATE_DIRECTORY, "news.erb.atom")
 renderer = ErbRenderer.new(erb_template, articles_view.binding)
 File.open(File.join(File.dirname(__FILE__), "news.atom"), 'w') { |io| renderer.render(io) }
