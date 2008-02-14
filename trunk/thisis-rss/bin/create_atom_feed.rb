3552b8811d2271851ea5cee17038aa8e3f7dba6f
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-02-14 08:44:13 +0000

    This code's a bit sloppy but it's generally working - I wanted to get it checked in before I start making any further changes to it.

--- trunk/thisis-rss/bin/create_atom_feed.rb
+++ trunk/thisis-rss/bin/create_atom_feed.rb
@@ -15,16 +15,25 @@ File.open(ArticlesFilename, 'w') unless File.exists?(ArticlesFilename) # Create 
 
 articles = Articles.retrieve_from_yaml_file(ArticlesFilename)
 
-html_filename = File.join(HTML_DIRECTORY, 'sample-news.html')
+# Dir[File.join(HTML_DIRECTORY, '*.html')].each do |html_filename|
+  # html_filename = File.join(HTML_DIRECTORY, 'sample-news.html')
 
-parser = DocumentParser.from_file(html_filename)
-parser.each_article_attributes do |article_attributes|
-  articles.add(article_attributes)
-end
+  # parser = DocumentParser.from_file(html_filename)
+  parser = DocumentParser.from_url('http://www.thisiskent.co.uk/displayNode.jsp?nodeId=250478&command=newPage')
+  duplicate_article_found = false
+  parser.each_article_attributes do |article_attributes|
+    article_added = articles.add(article_attributes)
+    duplicate_article_found = true unless article_added
+  end
+  unless duplicate_article_found
+    p parser.next_page_url
+  end
+
+# end
 
 articles.store_to_yaml_file(ArticlesFilename)
 
-articles_view = ArticlesView.new(articles, 2)
+articles_view = ArticlesView.new(articles, 15)
 
 erb_template = File.join(TEMPLATE_DIRECTORY, "news.erb.atom")
 renderer = ErbRenderer.new(erb_template, articles_view.binding)
