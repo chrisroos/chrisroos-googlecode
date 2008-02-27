2a4a8158497b30ad05333da00faafadc83c65220
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-02-27 09:12:05 +0000

    Slim the worker down a bit by moving some of the generally useful stuff into the environment.

---
+++ trunk/nat-rail-enq/config/environment.rb
@@ -0,0 +1,6 @@
+require File.join(File.dirname(__FILE__), '..', 'lib', 'message_parser')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'searcher')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'parser')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'twitter')
+
+MESSAGE_STORE = File.expand_path(File.join(File.dirname(__FILE__), '..', '/messages'))
