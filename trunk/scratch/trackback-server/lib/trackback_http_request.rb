13a6dca40175b42332df223d98e06f3f8feaad56
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-03-18 22:39:27 +0000

    The TrackbackHttpRequest now creates a trackback and checks for its validity, allowing us to move some logic out of the mongrel handlers.  I also fixed up the two tests: test_should_not_be_valid_if_http_method_is_not_as_per_the_spec and test_should_not_be_valid_if_content_type_is_not_as_per_the_spec as they weren't actually testing what I expected.

--- trunk/scratch/trackback-server/lib/trackback_http_request.rb
+++ trunk/scratch/trackback-server/lib/trackback_http_request.rb
@@ -1,19 +1,23 @@
 require File.join(File.dirname(__FILE__), 'trackback_http_request_http_method')
 require File.join(File.dirname(__FILE__), 'trackback_http_request_http_content_type')
+require File.join(File.dirname(__FILE__), 'trackback_http_request_trackback')
 
 class TrackbackHttpRequest
+  
+  attr_reader :trackback
   
   def initialize(http_request)
     @http_method = HttpMethod.new(http_request.params)
     @http_content_type = HttpContentType.new(http_request.params)
+    @trackback = Trackback.from_http_request(http_request)
   end
   
   def valid?
-    @http_method.valid? && @http_content_type.valid?
+    @http_method.valid? && @http_content_type.valid? && @trackback.valid?
   end
   
   def errors
-    [@http_method.error, @http_content_type.error].compact
+    [@http_method.error, @http_content_type.error, @trackback.error].compact
   end
   
 end
--- trunk/scratch/trackback-server/test/trackback_http_request_test.rb
+++ trunk/scratch/trackback-server/test/trackback_http_request_test.rb
@@ -3,44 +3,70 @@ require 'trackback_http_request'
 
 class TrackbackHttpRequestTest < Test::Unit::TestCase
   
-  def test_should_be_valid_if_both_content_type_and_http_method_are_as_per_the_spec
+  def test_should_be_valid_if_both_content_type_and_http_method_and_trackback_attributes_are_as_per_the_spec
     http_method = stub('http_method', :valid? => true)
     http_content_type = stub('http_content_type', :valid? => true)
+    trackback = stub('trackback', :valid? => true)
     TrackbackHttpRequest::HttpMethod.stubs(:new).returns(http_method)
     TrackbackHttpRequest::HttpContentType.stubs(:new).returns(http_content_type)
-    http_request = stub('http_request', :params => {})
+    TrackbackHttpRequest::Trackback.stubs(:new).returns(trackback)
+    http_request = stub('http_request', :params => {}, :body => stub('io', :read => ''))
     trackback_http_request = TrackbackHttpRequest.new(http_request)
     assert trackback_http_request.valid?
   end
   
   def test_should_not_be_valid_if_http_method_is_not_as_per_the_spec
-    TrackbackHttpRequest::HttpMethod.stubs(:valid?).returns(false)
-    TrackbackHttpRequest::HttpContentType.stubs(:valid?).returns(true)
-    http_request = stub('http_request', :params => {})
+    http_method = stub('http_method', :valid? => false)
+    http_content_type = stub('http_content_type', :valid? => true)
+    trackback = stub('trackback', :valid? => true)
+    TrackbackHttpRequest::HttpMethod.stubs(:new).returns(http_method)
+    TrackbackHttpRequest::HttpContentType.stubs(:new).returns(http_content_type)
+    TrackbackHttpRequest::Trackback.stubs(:new).returns(trackback)
+    http_request = stub('http_request', :params => {}, :body => stub('io', :read => ''))
     trackback_http_request = TrackbackHttpRequest.new(http_request)
     assert ! trackback_http_request.valid?
   end
   
   def test_should_not_be_valid_if_content_type_is_not_as_per_the_spec
-    TrackbackHttpRequest::HttpMethod.stubs(:valid?).returns(true)
-    TrackbackHttpRequest::HttpContentType.stubs(:valid?).returns(false)
-    http_request = stub('http_request', :params => {})
+    http_method = stub('http_method', :valid? => true)
+    http_content_type = stub('http_content_type', :valid? => false)
+    trackback = stub('trackback', :valid? => true)
+    TrackbackHttpRequest::HttpMethod.stubs(:new).returns(http_method)
+    TrackbackHttpRequest::HttpContentType.stubs(:new).returns(http_content_type)
+    TrackbackHttpRequest::Trackback.stubs(:new).returns(trackback)
+    http_request = stub('http_request', :params => {}, :body => stub('io', :read => ''))
     trackback_http_request = TrackbackHttpRequest.new(http_request)
     assert ! trackback_http_request.valid?
   end
   
-  def test_should_collect_errors_from_http_method_and_http_content_type_validators
-    http_method = stub('http_method', :valid? => false, :error => 'http-method-error')
-    http_content_type = stub('http_content_type', :valid? => false, :error => 'http-content-type-error')
+  def test_should_not_be_valid_if_trackback_attributes_are_not_as_per_the_spec
+    http_method = stub('http_method', :valid? => true)
+    http_content_type = stub('http_content_type', :valid? => true)
+    trackback = stub('trackback', :valid? => false)
     TrackbackHttpRequest::HttpMethod.stubs(:new).returns(http_method)
     TrackbackHttpRequest::HttpContentType.stubs(:new).returns(http_content_type)
-    http_request = stub('http_request', :params => {})
+    TrackbackHttpRequest::Trackback.stubs(:new).returns(trackback)
+    http_request = stub('http_request', :params => {}, :body => stub('io', :read => ''))
+    trackback_http_request = TrackbackHttpRequest.new(http_request)
+    assert ! trackback_http_request.valid?
+  end
+  
+  def test_should_collect_errors_from_http_method_and_http_content_type_and_trackback_validators
+    http_method = stub('http_method', :valid? => false, :error => 'http-method-error')
+    http_content_type = stub('http_content_type', :valid? => false, :error => 'http-content-type-error')
+    trackback = stub('trackback', :valid? => false, :error => 'trackback-error')
+    TrackbackHttpRequest::HttpMethod.stubs(:new).returns(http_method)
+    TrackbackHttpRequest::HttpContentType.stubs(:new).returns(http_content_type)
+    TrackbackHttpRequest::Trackback.stubs(:new).returns(trackback)
+    http_request = stub('http_request', :params => {}, :body => stub('io', :read => ''))
     trackback_http_request = TrackbackHttpRequest.new(http_request)
     trackback_http_request.valid?
-    assert_equal ['http-method-error', 'http-content-type-error'], trackback_http_request.errors
+    assert_equal ['http-method-error', 'http-content-type-error', 'trackback-error'], trackback_http_request.errors
   end
 
 end
+
+require 'stringio'
 
 class TrackbackHttpRequestIntegrationTest < Test::Unit::TestCase
   
@@ -49,7 +75,8 @@ class TrackbackHttpRequestIntegrationTest < Test::Unit::TestCase
       'REQUEST_METHOD' => 'POST',
       'HTTP_CONTENT_TYPE' => 'application/x-www-form-urlencoded'
     }
-    http_request = stub('http_request', :params => http_params)
+    http_body = StringIO.new('url=permalink')
+    http_request = stub('http_request', :params => http_params, :body => http_body)
     trackback_http_request = TrackbackHttpRequest.new(http_request)
     assert trackback_http_request.valid?
     assert trackback_http_request.errors.empty?
