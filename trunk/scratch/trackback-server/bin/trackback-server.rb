a079183f02af2271c1909bcf8f1c47f68cc57c77
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-03-19 09:50:51 +0000

    Move the xml templates out of the server and into their own erb files.  Move more of the persistence/retrieval logic out of the trackback server and into the Trackbacks object.

--- trunk/scratch/trackback-server/bin/trackback-server.rb
+++ trunk/scratch/trackback-server/bin/trackback-server.rb
@@ -1,3 +1,6 @@
+DATA_DIRECTORY = File.join(File.dirname(__FILE__), '..', 'data')
+TEMPLATE_DIRECTORY = File.join(File.dirname(__FILE__), '..', 'templates')
+
 require 'rubygems'
 require 'mongrel'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'trackback_http_request')
@@ -5,19 +8,13 @@ require File.join(File.dirname(__FILE__), '..', 'lib', 'trackback_renderer')
 require File.join(File.dirname(__FILE__), '..', 'lib', 'trackbacks')
 require File.join(File.dirname(__FILE__), '..', 'lib', 'erb_renderer')
 
-TRACKBACK_FILE = File.join(File.dirname(__FILE__), '..', 'data', 'trackbacks.yml')
-unless File.exists?(TRACKBACK_FILE)
-  File.open(TRACKBACK_FILE, 'w') { |f| f.puts [].to_yaml }
-end
-
-TRACKBACKS_TEMPLATE = File.join(File.dirname(__FILE__), '..', 'templates', 'trackbacks.html.erb')
-
 class TrackbackListHandler < Mongrel::HttpHandler
   def process(request, response)
-    trackbacks = Trackbacks.from_yaml_file(TRACKBACK_FILE)
+    trackbacks = Trackbacks.find_all
     response.start do |head, out|
       head['Content-Type'] = 'text/html'
-      ErbRenderer.new(TRACKBACKS_TEMPLATE, binding).render(out)
+      template = File.join(TEMPLATE_DIRECTORY, 'trackbacks.html.erb')
+      ErbRenderer.new(template, binding).render(out)
     end
   end
 end
@@ -28,28 +25,14 @@ class TrackbackHandler < Mongrel::HttpHandler
 
     if trackback_http_request.valid?
       data = trackback_http_request.trackback.to_hash
-      trackbacks = Trackbacks.from_yaml_file(TRACKBACK_FILE)
-      trackbacks.add(data)
-      trackbacks.to_yaml_file(TRACKBACK_FILE)
-
-      xml_response = <<-EndXml
-<?xml version="1.0" encoding="utf-8"?>
-<response>
-  <error>0</error>
-</response>
-EndXml
+      Trackbacks.create(data)
+      template = File.join(TEMPLATE_DIRECTORY, 'trackback_success.xml.erb')
     else
-      xml_response = <<-EndXml
-<?xml version="1.0" encoding="utf-8"?> 
-<response> 
-  <error>1</error> 
-  <message>#{trackback_http_request.errors.join(', ')}</message> 
-</response>
-      EndXml
+      template = File.join(TEMPLATE_DIRECTORY, 'trackback_failure.xml.erb')
     end
     
     response.start do |head, out|
-      out << xml_response
+      ErbRenderer.new(template, binding).render(out)
     end
   end
 end
