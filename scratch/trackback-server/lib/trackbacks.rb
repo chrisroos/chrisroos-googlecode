1890764ffab3289e03a45941cf4af48c0b57e47b
Chris Roos <chris@seagul.co.uk>
2008-03-19 09:50:51 +0000

    Move the xml templates out of the server and into their own erb files.  Move more of the persistence/retrieval logic out of the trackback server and into the Trackbacks object.

--- scratch/trackback-server/lib/trackbacks.rb
+++ scratch/trackback-server/lib/trackbacks.rb
@@ -2,8 +2,24 @@ require 'yaml'
 
 class Trackbacks
   
-  def self.from_yaml_file(filename)
-    new YAML.load_file(filename)
+  DataLocation = File.join(DATA_DIRECTORY, 'trackbacks.yml')
+  
+  class << self
+    def create(trackback)
+      trackbacks = from_yaml_file(DataLocation)
+      trackbacks.add(trackback)
+      trackbacks.save
+    end
+  
+    def find_all
+      from_yaml_file(DataLocation)
+    end
+  
+    def from_yaml_file(filename)
+      File.open(DataLocation, 'w') { |f| f.puts [].to_yaml } unless File.exists?(DataLocation)
+      new YAML.load_file(filename)
+    end
+    private :from_yaml_file
   end
   
   def initialize(trackbacks = [])
@@ -18,8 +34,13 @@ class Trackbacks
     @trackbacks.each(&blk)
   end
   
+  def save
+    to_yaml_file(DataLocation)
+  end
+  
   def to_yaml_file(filename)
     File.open(filename, 'w') { |f| f.puts(@trackbacks.to_yaml) }
   end
+  private :to_yaml_file
   
 end
