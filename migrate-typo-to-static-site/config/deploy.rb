2f7c0fa08ad838550ad98f682f817c7c333c6064
Chris Roos <chris@seagul.co.uk>
2008-03-22 09:15:45 +0000

    Capify my blog so that I can deploy easily with capistrano.

---
+++ migrate-typo-to-static-site/config/deploy.rb
@@ -0,0 +1,9 @@
+set :application, "blog.seagul.co.uk"
+set :repository,  "http://chrisroos.googlecode.com/svn/trunk/migrate-typo-to-static-site"
+
+set :deploy_to, "/home/chrisroos/www/#{application}"
+set :deploy_via, :copy
+
+role :app, "jail0093.vps.exonetric.net"
+role :web, "jail0093.vps.exonetric.net"
+role :db,  "jail0093.vps.exonetric.net", :primary => true
