66838b1b39b9fc1f1f384b60936fdf5a75c348ee
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-03-22 11:48:48 +0000

    Create the apache config as part of the deployment rather than hard-coding the locations in the config files themselves.

---
+++ trunk/the-local-paper/config/deploy.rb
@@ -0,0 +1,23 @@
+set :application, "the-local-paper.co.uk"
+set :repository,  "http://chrisroos.googlecode.com/svn/trunk/the-local-paper"
+
+set :deploy_to, "/home/chrisroos/www/#{application}"
+set :deploy_via, :copy
+
+role :app, "jail0093.vps.exonetric.net"
+role :web, "jail0093.vps.exonetric.net"
+role :db,  "jail0093.vps.exonetric.net", :primary => true
+
+desc "Create the apache config file that allows us to actually host this site"
+task :create_apache_config do
+  public_directory = "#{current_path}/public"
+  log_directory = "#{current_path}/log"
+  template = File.open(File.join(File.dirname(__FILE__), *%w[apache.config.erb])) { |f| f.read }
+  erb = ERB.new(template)
+  put erb.result(binding), "#{current_path}/config/apache.config"
+end
+
+desc "Restart the Apache webserver"
+task :restart_apache do
+  sudo "apachectl restart"
+end
