e43c9f9020899ed01469a6f505b724eb0c00ce0b
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-03-22 09:35:52 +0000

    Add a task that creates the relevant apache config for this site.

--- trunk/migrate-typo-to-static-site/config/deploy.rb
+++ trunk/migrate-typo-to-static-site/config/deploy.rb
@@ -7,3 +7,12 @@ set :deploy_via, :copy
 role :app, "jail0093.vps.exonetric.net"
 role :web, "jail0093.vps.exonetric.net"
 role :db,  "jail0093.vps.exonetric.net", :primary => true
+
+desc "Create the apache config file that allows us to actually host this site"
+task :create_apache_config do
+  public_directory = "#{deploy_to}/public"
+  log_directory = "#{deploy_to}/log"
+  template = File.open(File.join(File.dirname(__FILE__), *%w[apache.config.erb])) { |f| f.read }
+  erb = ERB.new(template)
+  put erb.result(binding), "#{current_path}/config/apache.config"
+end
