4348ef70e96bfb5d9e5484aa6c72a8bf607234ba
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-03-25 21:01:48 +0000

    Capify the app to make for easier deployment.  In fact, it's running right now on my new jail.  Yay.

---
+++ trunk/scratch/trackback-server/config/deploy.rb
@@ -0,0 +1,42 @@
+default_environment['PATH'] = "/usr/local/bin:$PATH"
+
+set :application, "trackbacks.seagul.co.uk"
+set :repository,  "http://chrisroos.googlecode.com/svn/trunk/scratch/trackback-server"
+
+set :deploy_to, "/home/chrisroos/www/#{application}"
+set :deploy_via, :copy
+
+role :app, "jail0093.vps.exonetric.net"
+role :web, "jail0093.vps.exonetric.net"
+role :db,  "jail0093.vps.exonetric.net", :primary => true
+
+desc "Create the apache config file that allows us to actually host this site"
+task :create_apache_config do
+  public_directory = "#{current_path}/public"
+  log_directory = "#{current_path}/log"
+  template = File.open(File.join(File.dirname(__FILE__), *%w[apache.config.erb])) { |f| f.read }
+  erb = ERB.new(template)
+  put erb.result(binding), "#{current_path}/config/apache.config"
+end
+
+desc "Create a symlink to the apache config so that all config files end up in the same directory which allows us to restart apache and get it to load all config files in that directory."
+task :create_symlink_to_apache_config do
+  run "ln -s #{current_path}/config/apache.config #{deploy_to}.config"
+end
+
+desc "Restart the Apache webserver"
+task :restart_apache do
+  sudo "apachectl restart"
+end
+
+desc "Start the trackback server"
+task :start_trackback_server do
+  run "#{current_path}/bin/trackback-server.rb"
+end
+
+desc "Clear current trackbacks"
+task :clear_trackbacks do
+  run "rm #{current_path}/data/trackbacks.yml"
+end
+
+# Add tasks to stop trackback server and clear trackbacks from production
