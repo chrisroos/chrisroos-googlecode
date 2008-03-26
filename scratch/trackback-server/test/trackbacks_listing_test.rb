bd3591c924fc335181d0791abc4c383bab838890
Chris Roos <chris@seagul.co.uk>
2008-03-26 14:29:01 +0000

    We should be able to display trackbacks that were submitted with the minimum data possible, as per the spec.

---
+++ scratch/trackback-server/test/trackbacks_listing_test.rb
@@ -0,0 +1,32 @@
+require File.join(File.dirname(__FILE__), 'test_helper')
+require 'weekday_suffix'
+require 'erb'
+require 'trackback_renderer'
+require 'hpricot'
+
+TEMPLATE_DIRECTORY = File.join(File.dirname(__FILE__), '..', 'templates')
+
+class TrackbacksListingTest < Test::Unit::TestCase
+  
+  def test_should_render_ok_when_only_the_required_trackback_data_is_supplied
+    trackback = {
+      'blog_name' => nil,
+      'url' => 'url',
+      'received_at' => Time.now,
+      'title' => nil,
+      'excerpt' => nil
+    }
+    trackbacks = [trackback]
+    template = File.open(File.join(TEMPLATE_DIRECTORY, 'trackbacks.html.erb')) { |f| f.read }
+    erb = ERB.new(template)
+    rendered_template = erb.result(binding)
+    
+    doc = Hpricot(rendered_template)
+    
+    assert_equal 'url', (doc/'a.trackbackPermalink').first.attributes['href']
+    assert_equal 'Anonymous', (doc/'a.trackbackPermalink').first.innerText
+    assert (doc/'p.trackbackTitle').empty?
+    assert (doc/'p.trackbackExcerpt').empty?
+  end
+  
+end
