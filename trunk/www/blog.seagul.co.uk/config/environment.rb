8d14b79abe38fb5fbe0a34a1341e6695cd9993d0
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-04-01 23:10:10 +0000

    These final changes to the Trackback model (it no longer derives from AR::Base) allow us to generate the site without the database at all (I've removed the database connection an' everythin').

--- trunk/www/blog.seagul.co.uk/config/environment.rb
+++ trunk/www/blog.seagul.co.uk/config/environment.rb
@@ -17,5 +17,5 @@ $: << File.join(MIGRATOR_ROOT, 'lib')
 database_config = YAML.load_file(File.join(File.dirname(__FILE__), 'database.yml'))
 database_config = database_config['production']
 
-ActiveRecord::Base.establish_connection(database_config)
-ActiveRecord::Base.logger = Logger.new(StringIO.new)
+# ActiveRecord::Base.establish_connection(database_config)
+# ActiveRecord::Base.logger = Logger.new(StringIO.new)
--- trunk/www/blog.seagul.co.uk/site-generation/models/trackback.rb
+++ trunk/www/blog.seagul.co.uk/site-generation/models/trackback.rb
@@ -1,6 +1,12 @@
-class Trackback < ActiveRecord::Base
+class Trackback
   
-  belongs_to :article
+  attr_accessor :published_at, :blog_name, :url, :excerpt, :title
+  
+  def initialize(attributes)
+    attributes.each do |attribute, value|
+      __send__("#{attribute}=", value)
+    end
+  end
   
   def formatted_published_date
     published_at.strftime("%a, %d %b %Y %H:%M:%S")
