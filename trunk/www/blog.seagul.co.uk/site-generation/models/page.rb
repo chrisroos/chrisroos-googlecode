13c2c7cb0226c7e4331bcbe70f6d891d49dca58f
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-04-01 22:56:10 +0000

    Extract from the evils of the database.  Yay.  The site is now generated by storing the articles (et al) in memory and then writing them out to our html layouts.

--- trunk/www/blog.seagul.co.uk/site-generation/models/page.rb
+++ trunk/www/blog.seagul.co.uk/site-generation/models/page.rb
@@ -1,4 +1,22 @@
-class Page < ActiveRecord::Base
+class Page
+  
+  attr_accessor :name, :title, :body, :created_at, :updated_at
+  
+  def initialize(attributes)
+    attributes.each do |attribute, value|
+      __send__("#{attribute}=", value)
+    end
+  end
+  
+  def self.find_all
+    @pages ||= (
+      pages_dir = File.join(File.dirname(__FILE__), *%w[.. .. content pages])
+      Dir[File.join(pages_dir, '*.yml')].collect do |page_filename|
+        page_attributes = YAML.load_file(page_filename)
+        Page.new(page_attributes)
+      end
+    )
+  end
   
   def formatted_created_date
     created_at.strftime("%a, %d %b %Y %H:%M:%S")
