d70c21d00fea38fedfd9ee7db58e4b25a1f84ebf
Chris Roos <chris@seagul.co.uk>
2008-04-08 13:23:39 +0000

    I haven't sorted the .htaccess redirect rules to allow us to serve requests for files that don't have html extensions.  Until that time I don't want the sitemap to list the pool pages I have.

--- www/public-swimming-pools.co.uk/bin/generate_sitemap.rb
+++ www/public-swimming-pools.co.uk/bin/generate_sitemap.rb
@@ -9,11 +9,11 @@ builder.urlset(:xmlns => 'http://www.sitemaps.org/schemas/sitemap/0.9') do
   builder.url do
     builder.loc 'http://public-swimming-pools.co.uk/swimming-pools.kml'
   end
-  SwimmingPools.find_all.each do |swimming_pool|
-    builder.url do 
-      builder.loc "http://public-swimming-pools.co.uk/#{swimming_pool.permalink}"
-    end
-  end
+  # SwimmingPools.find_all.each do |swimming_pool|
+  #   builder.url do 
+  #     builder.loc "http://public-swimming-pools.co.uk/#{swimming_pool.permalink}"
+  #   end
+  # end
 end
 
 File.open(File.join(File.dirname(__FILE__), *%w[.. public sitemap.xml]), 'w') { |f| f.puts(sitemap_buffer.string) }
