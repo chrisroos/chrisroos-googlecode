75328b92169e56462d9f276c817101089b2e49f7
Chris Roos <chris@seagul.co.uk>
2008-04-09 21:04:34 +0000

    Hmm, a few changes in here.  Some CSS to make it look pwetty.  An about page.  Tidied up swimming pool page.

--- www/public-swimming-pools.co.uk/bin/generate_site.rb
+++ www/public-swimming-pools.co.uk/bin/generate_site.rb
@@ -23,39 +23,72 @@ __END__
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
+    <link rel="stylesheet" href="/stylesheets/swimming-pools.css" type="text/css" media="screen" charset="utf-8" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title><%= h(swimming_pool.name) %></title>
   </head>
   <body>
-    <h1><%= h(swimming_pool.name) %></h1>
     
-    <dl>
-      <dt>Address</dt>
-      <dd class="address"><%= h(swimming_pool.address) %></dd>
-      <dt>Postcode</dt>
-      <dd class="postcode"><%= h(swimming_pool.postcode) %></dd>
-      <dt>Latitude</dt>
-      <dd class="latitude"><%= h(swimming_pool.latitude) %></dd>
-      <dt>Longitude</dt>
-      <dd class="longitude"><%= h(swimming_pool.longitude) %></dd>
-      <dt>Phone</dt>
-      <dd class="phone"><%= h(swimming_pool.phone) %></dd>
-      <dt>Fax</dt>
-      <dd class="fax"><%= h(swimming_pool.fax) %></dd>
-      <dt>Website</dt>
-      <dd class="website"><a href="<%= h(swimming_pool.url) %>"><%= h(swimming_pool.url) %></a></dd>
-      <dt>Email</dt>
-      <dd class="email"><a href="mailto:<%= h(swimming_pool.email) %>"><%= h(swimming_pool.email) %></a></dd>
-    </dl>
+    <h1><a href="/">Home</a> - <%= h(swimming_pool.name) %></h1>
+  
+    <div class="mapContainer">
+      <% img_url = 'http://maps.google.com/staticmap?' %>
+      <% img_url_params = ["center=#{swimming_pool.latitude},#{swimming_pool.longitude}"] %>
+      <% img_url_params << 'zoom=14' %>
+      <% img_url_params << 'size=300x300' %>
+      <% img_url_params << "markers=#{swimming_pool.latitude},#{swimming_pool.longitude}" %>
+      <% img_url_params << 'key=ABQIAAAAKHjf8pvj0mv3o07jD0Tc5RQaqNW12GnHBM9UqBhN-tKTStROsxS_YKqdsgFyjjinVKdFKNOHhZmPOQ' %>
+      <% img_url = img_url + img_url_params.join('&') %>
+      <img width="200px" height="200px" src="<%= h(img_url) %>" alt="<%= h(swimming_pool.name) %>" />
+    </div>
+  
+    <table>
+      <tr>
+        <td class="attribute">Address</td>
+        <td class="address"><%= h(swimming_pool.address) %></td>
+      </tr>
+      <tr>
+        <td class="attribute">Postcode</td>
+        <td class="postcode"><%= h(swimming_pool.postcode) %></td>
+      </tr>
+      <tr>
+        <td class="attribute">Phone</td>
+        <td class="phone"><%= h(swimming_pool.phone) %></td>
+      </tr>
+      <tr>
+        <td class="attribute">Fax</td>
+        <td class="fax"><%= h(swimming_pool.fax) %></td>
+      </tr>
+      <tr>
+        <td class="attribute">Email</td>
+        <td><a class="email" href="mailto:<%= h(swimming_pool.email) %>"><%= h(swimming_pool.email) %></a></td>
+      </tr>
+      <tr>
+        <td class="attribute">Website</td>
+        <td><a class="website" href="<%= h(swimming_pool.website) %>"><%= h(swimming_pool.website_domain) %></a></td>
+      </tr>
+      <tr>
+        <td class="attribute">Latitude</td>
+        <td class="latitude"><%= h(swimming_pool.latitude) %></td>
+      </tr>
+      <tr>
+        <td class="attribute">Longitude</td>
+        <td class="longitude"><%= h(swimming_pool.longitude) %></td>
+      </tr>
+    </table>
+  
+    <div class="footer">
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+    </div>
     
-    <% img_url = 'http://maps.google.com/staticmap?' %>
-    <% img_url_params = ["center=#{swimming_pool.latitude},#{swimming_pool.longitude}"] %>
-    <% img_url_params << 'zoom=14' %>
-    <% img_url_params << 'size=300x300' %>
-    <% img_url_params << "markers=#{swimming_pool.latitude},#{swimming_pool.longitude}" %>
-    <% img_url_params << 'key=ABQIAAAAKHjf8pvj0mv3o07jD0Tc5RQaqNW12GnHBM9UqBhN-tKTStROsxS_YKqdsgFyjjinVKdFKNOHhZmPOQ' %>
-    <% img_url = img_url + img_url_params.join('&') %>
-    <p><img src="<%= h(img_url) %>" width="300" height="300" alt="<%= h(swimming_pool.name) %>" /></p>
+    <!-- <div class="footer">
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="http://groups.google.com/group/public-swimming-pools">Forum</a></li>
+      </ul>
+    </div> -->
     
     <script type="text/javascript">
       var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
