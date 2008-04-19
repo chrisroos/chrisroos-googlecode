8098a5c6156aef22d8e4ff4cd3dacf39ec2fed51
Chris Roos <chris@seagul.co.uk>
2008-04-19 17:54:36 +0000

    Better (imo) namespacing and naming of the tasks.

--- capistrano-recipes/static-sites/recipes/static-site.rb
+++ capistrano-recipes/static-sites/recipes/static-site.rb
@@ -1,7 +1,7 @@
-namespace :static_sites do
+namespace :apache do
   
   desc "Create the apache config file that allows us to actually host this site"
-  task :create_apache_config do
+  task :create_config do
     public_directory = "#{current_path}/public"
     log_directory = "#{current_path}/log"
     template = File.open(File.join(File.dirname(__FILE__), *%w[.. .. .. .. config apache.config.erb])) { |f| f.read }
@@ -10,17 +10,21 @@ namespace :static_sites do
   end
 
   desc "Create a symlink to the apache config so that all config files end up in the same directory which allows us to restart apache and get it to load all config files in that directory."
-  task :create_symlink_to_apache_config do
+  task :symlink_config do
     run "ln -fs #{shared_path}/apache.config #{deploy_to}.config"
   end
 
   desc "Restart the Apache webserver"
-  task :restart_apache do
+  task :restart do
     sudo "apachectl restart"
   end
+  
+end
 
+namespace :site do
+  
   desc "Updates the site by svn up'ing, rather than checking out the entire site content and uploading (which deploy:update does)"
-  task :update_site do
+  task :update do
     run "cd #{current_path} && svn up"
   end
   
