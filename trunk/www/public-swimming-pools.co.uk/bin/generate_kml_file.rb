ba42e894e2fceae1862c01bac2087b2fda5db55f
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-04-19 17:49:45 +0000

    Improve the description of the swimming pools in the kml file.

--- trunk/www/public-swimming-pools.co.uk/bin/generate_kml_file.rb
+++ trunk/www/public-swimming-pools.co.uk/bin/generate_kml_file.rb
@@ -1,7 +1,11 @@
 require 'builder'
+require 'erb'
 require File.join(File.dirname(__FILE__), *%w[.. lib swimming_pools])
 
 kml_buffer = StringIO.new
+
+template = DATA.read
+erb = ERB.new(template)
 
 builder = Builder::XmlMarkup.new(:target => kml_buffer, :indent => 2)
 builder.instruct!
@@ -10,7 +14,9 @@ builder.kml(:xmlns => 'http://earth.google.com/kml/2.2') do
     SwimmingPools.find_all.each do |swimming_pool|
       builder.Placemark do 
         builder.name(swimming_pool.name)
-        builder.description(swimming_pool.address)
+        builder.description do
+          builder.cdata!(erb.result(binding))
+        end
         builder.Point do
           builder.coordinates(swimming_pool.kml_coordinates)
         end
@@ -20,3 +26,8 @@ builder.kml(:xmlns => 'http://earth.google.com/kml/2.2') do
 end
 
 File.open(File.join(File.dirname(__FILE__), *%w[.. public swimming-pools.kml]), 'w') { |f| f.puts(kml_buffer.string) }
+
+__END__
+<a href="<%= swimming_pool.website %>"><%= swimming_pool.name %></a><br/>
+<%= [swimming_pool.address, swimming_pool.postcode].join(', ') %><br/>
+<%= swimming_pool.phone %><br/>
