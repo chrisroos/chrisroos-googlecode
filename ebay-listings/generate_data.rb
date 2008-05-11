25954508e621bf196c017d4a468a2dc2531d3b84
Chris Roos <chris@seagul.co.uk>
2008-05-11 13:26:56 +0000

    Nicer styling.  Persist listing data and resulting html.

--- ebay-listings/generate_data.rb
+++ ebay-listings/generate_data.rb
@@ -1,7 +1,33 @@
 require 'flickr'
 
-dimensions, weight, tag = ARGV
+name = ARGV.shift
+dimensions = ARGV.shift
+weight = ARGV.shift
+tag = ARGV.shift
+
+unless name && dimensions && weight
+  puts "Usage: ruby generate_data.rb name-of-item dimensions (wxhxd) weight [tag]"
+  exit
+end
+
+description = ''
+while line = gets
+  description << line
+end
+
 w, h, d = dimensions.split('x').collect { |x| Float(x) }
 volumetric_weight = (w * h * d) / 6000
 photos = Photos.get(tag)
+data = {
+  :name => name,
+  :dimensions => dimensions,
+  :weight => weight,
+  :volumetric_weight => volumetric_weight,
+  :tag => tag,
+  :photos => photos,
+  :description => description
+}
 
+require 'yaml'
+
+File.open(File.join('listings', name), 'w') { |f| f.puts(data.to_yaml) }
