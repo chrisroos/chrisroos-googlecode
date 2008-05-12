c06a5e88c36ca971dcdfa76b26a2de959bac32cd
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-05-12 13:34:49 +0000

    Move a 'test' out of the implementation and into the test class.

--- trunk/pwdhash-rb/pwdhash_test.rb
+++ trunk/pwdhash-rb/pwdhash_test.rb
@@ -8,5 +8,11 @@ class PwdHashTest < Test::Unit::TestCase
     actual = get_hashed_password('password', 'realm')
     assert_equal expected, actual
   end
-  
+
+  def test_should_apply_constraints
+    expected = '1JMstxYHYz'
+    actual = apply_constraints(hash='MstxYHYzGq0jEjzfTbaFrQ', size=10, nonalphanumeric=false)
+    assert_equal expected, actual
+  end
+
 end
