c23fcfecfd7ef9fe3dfc00dadfb526339ed508f7
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-05-28 08:24:48 +0000

    Extract the smile domain in order to reduce a little duplication.

--- trunk/smile.co.uk/smile.rb
+++ trunk/smile.co.uk/smile.rb
@@ -16,14 +16,18 @@ def pages_path(page_name)
   File.join(File.dirname(__FILE__), 'pages', page_name)
 end
 
+def smile_domain
+  'https://welcome23.smile.co.uk'
+end
+
 #*** Start page
-start_page = `curl "https://welcome23.smile.co.uk/SmileWeb/start.do" -c"#{cookie_path}"`
+start_page = `curl "#{smile_domain}/SmileWeb/start.do" -c"#{cookie_path}"`
 File.open(pages_path('start_page.html'), 'w') { |f| f.puts(start_page) }
 doc = Hpricot(start_page)
 token = (doc/'form'/'input[@name=org.apache.struts.taglib.html.TOKEN]').first.attributes['value']
 
 #*** Security number page
-security_number_page = `curl "https://welcome23.smile.co.uk/SmileWeb/login.do" \
+security_number_page = `curl "#{smile_domain}/SmileWeb/login.do" \
 -d"org.apache.struts.taglib.html.TOKEN=#{token}" \
 -d"sortCode=#{SecurityDetails['sortCode']}" \
 -d"accountNumber=#{SecurityDetails['accountNumber']}" \
@@ -36,7 +40,7 @@ first_security_digit = SecurityDetails['securityCode'].__send__(first_passcode_d
 second_security_digit = SecurityDetails['securityCode'].__send__(second_passcode_digit)
 token = (doc/'form'/'input[@name=org.apache.struts.taglib.html.TOKEN]').first.attributes['value']
 
-curl_cmd = %%curl "https://welcome23.smile.co.uk/SmileWeb/passcode.do" \
+curl_cmd = %%curl "#{smile_domain}/SmileWeb/passcode.do" \
 -d"org.apache.struts.taglib.html.TOKEN=#{token}" \
 -d"firstPassCodeDigit=#{first_security_digit}" \
 -d"secondPassCodeDigit=#{second_security_digit}" \
@@ -47,7 +51,7 @@ doc = Hpricot(security_question_page)
 question = (doc/'form'/'input[@type=password]').first.attributes['name']
 token = (doc/'form'/'input[@name=org.apache.struts.taglib.html.TOKEN]').first.attributes['value']
 
-curl_cmd = %%curl "https://welcome23.smile.co.uk/SmileWeb/loginSpi.do" \
+curl_cmd = %%curl "#{smile_domain}/SmileWeb/loginSpi.do" \
 -d"org.apache.struts.taglib.html.TOKEN=#{token}" \
 -b"#{cookie_path}"%
 if question == 'memorableDay'
@@ -65,7 +69,7 @@ File.open(pages_path('accounts-overview.html'), 'w') { |f| f.puts(accounts_overv
 doc = Hpricot(accounts_overview_page)
 current_account_url = (doc/'a[@title=click here to go to recent items]').first.attributes['href']
 
-curl_cmd = %%curl "https://welcome23.smile.co.uk#{current_account_url}" \
+curl_cmd = %%curl "#{smile_domain}#{current_account_url}" \
 -b"#{cookie_path}"% 
 current_account_overview_page = `#{curl_cmd}`
 File.open(pages_path('current-account-overview.html'), 'w') { |f| f.puts(current_account_overview_page) }
