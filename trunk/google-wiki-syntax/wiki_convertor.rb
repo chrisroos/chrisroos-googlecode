99f942bb854c9536b602c9e5a62467f09e05515d
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-07-29 13:17:24 +0000

    A script that will convert a directory containing google wiki content into a directory of html content.

---
+++ trunk/google-wiki-syntax/wiki_convertor.rb
@@ -0,0 +1,41 @@
+#! /usr/bin/env ruby
+
+require 'lib/wiki_syntax.rb'
+
+module GoogleWiki
+  class WikiFolder
+    def initialize(directory)
+      @directory = directory
+    end
+    def each
+      Dir[File.join(@directory, '*.wiki')].each do |wiki_file|
+        yield WikiDocument.new(wiki_file)
+      end
+    end
+  end
+
+  class WikiDocument
+    attr_reader :filename
+    def initialize(filename)
+      @filename = filename
+      @document_name = File.basename(@filename, '.wiki')
+    end
+    def to_html_document(html_folder)
+      wiki_markup = File.read(@filename)
+      html = WikiSyntax.new(wiki_markup).to_html_document(@document_name)
+      File.open(File.join(html_folder, "#{@document_name}.html"), 'w') { |f| f.puts(html) }
+    end
+  end
+end
+
+wiki_folder = ARGV.shift
+html_folder = ARGV.shift
+unless wiki_folder and html_folder
+  puts "Usage: wiki_convertor.rb <wiki_folder> <html_folder>"
+  exit
+end
+
+GoogleWiki::WikiFolder.new(wiki_folder).each do |wiki_document|
+  puts "Converting #{wiki_document.filename}..."
+  wiki_document.to_html_document(html_folder)
+end
