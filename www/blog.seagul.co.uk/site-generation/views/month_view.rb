e99046b7beb0724a9c53b64df715d4ab16b1fdea
Chris Roos <chris@seagul.co.uk>
2008-07-31 13:21:21 +0000

    I don't want my monthly archive pages appearing in search engine indexes.

--- www/blog.seagul.co.uk/site-generation/views/month_view.rb
+++ www/blog.seagul.co.uk/site-generation/views/month_view.rb
@@ -2,4 +2,7 @@ class MonthView < ArticlesView
   def page_title
     "Posts published in #{@articles_container.published_date.strftime("%B %Y")}"
   end
+  def robots_meta_tag
+    %%<meta name="robots" content="noindex" />%
+  end
 end
