eb0d6f338217fe0b65045cd44b4ee92b64290712
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-07-31 13:23:39 +0000

    I don't want my yearly archive pages appearing in search engine indexes.

--- trunk/www/blog.seagul.co.uk/site-generation/views/year_view.rb
+++ trunk/www/blog.seagul.co.uk/site-generation/views/year_view.rb
@@ -2,4 +2,7 @@ class YearView < ArticlesView
   def page_title
     "Posts published in #{@articles_container.published_date.strftime("%Y")}"
   end
+  def robots_meta_tag
+    %%<meta name="robots" content="noindex" />%
+  end
 end
