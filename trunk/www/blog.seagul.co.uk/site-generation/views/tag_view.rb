a20f7afc98fdd50d91abb251e8bf47c8f332c88f
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-07-31 13:30:16 +0000

    I don't want my tag archive pages appearing in search engine indexes.

--- trunk/www/blog.seagul.co.uk/site-generation/views/tag_view.rb
+++ trunk/www/blog.seagul.co.uk/site-generation/views/tag_view.rb
@@ -2,4 +2,7 @@ class TagView < ArticlesView
   def page_title
     "Posts tagged #{@articles_container.name}"
   end
+  def robots_meta_tag
+    %%<meta name="robots" content="noindex" />%
+  end
 end
