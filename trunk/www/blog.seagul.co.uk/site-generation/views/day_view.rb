3c3180bb42dd56e0427733db0a6c4f387c2bec48
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-08-09 18:45:09 +0000

    These views are no longer required (they were for the tags and archive pages).

--- trunk/www/blog.seagul.co.uk/site-generation/views/day_view.rb
+++
@@ -1,8 +0,0 @@
-class DayView < ArticlesView
-  def page_title
-    "Posts published on #{@articles_container.published_date.strftime("%a, %d %B %Y")}"
-  end
-  def robots_meta_tag
-    %%<meta name="robots" content="noindex" />%
-  end
-end
--- trunk/www/blog.seagul.co.uk/site-generation/views/tag_view.rb
+++
@@ -1,8 +0,0 @@
-class TagView < ArticlesView
-  def page_title
-    "Posts tagged #{@articles_container.name}"
-  end
-  def robots_meta_tag
-    %%<meta name="robots" content="noindex" />%
-  end
-end
