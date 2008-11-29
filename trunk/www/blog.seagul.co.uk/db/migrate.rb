45bb941dfc71a30b5bb4284448f8a21c9a5485a3
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-11-29 11:38:04 +0000

    These migrations were only useful for converting the existing database backed blog content to the new static magic.

--- trunk/www/blog.seagul.co.uk/db/migrate.rb
+++
@@ -1,7 +0,0 @@
-require File.join(File.dirname(__FILE__), '..', 'config', 'environment')
-require 'active_record'
-require 'yaml'
-
-warn "Make sure you update schema_info to 0, or drop the table, before running these migrations for the first time.  Otherwise, they just won't work..."
-
-ActiveRecord::Migrator.migrate(File.join(File.dirname(__FILE__), 'migrate'))
