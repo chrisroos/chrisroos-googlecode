85ab0f19aa0df4cb2543507a29e8c78b04ce1220
Chris Roos <chris@seagul.co.uk>
2008-12-10 16:41:24 +0000

    The code in here was only required to get my legacy comments into a state that could be uploaded to disqus.  Now that that's done, this folder is unnecessary.

--- www/blog.seagul.co.uk/move_legacy_comments_to_disqus/upload-legacy-comments-to-disqus.rb
+++
@@ -1,9 +0,0 @@
-require File.join(File.dirname(__FILE__), 'disqus')
-
-my_blog_forum = get_forum('deferred-until-inspiration-hits')
-my_blog_forum_key = get_forum_api_key(my_blog_forum)
-
-Dir[File.join(COMMENTS_DIR, '*.txt')].each do |comment_file|
-  comment = YAML.load(File.read(comment_file))
-  create_comment(my_blog_forum_key, comment)
-end
