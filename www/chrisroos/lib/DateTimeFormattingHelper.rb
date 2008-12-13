22a41718b45f64f589c065b301df89a188a38ba6
Chris Roos <chris@seagul.co.uk>
2008-12-13 13:44:56 +0000

    Bugger.  This should've been committed in changeset:1205.

---
+++ www/chrisroos/lib/DateTimeFormattingHelper.rb
@@ -0,0 +1,9 @@
+# breadcrumbs.rb
+
+module DateTimeFormattingHelper
+  def formatted_created_date(page)
+    page.created_at.strftime("%a, %d %b %Y %H:%M:%S")
+  end
+end
+
+Webby::Helpers.register(DateTimeFormattingHelper)
