1f74346e007e053dc2f7b36bd8358573227eb400
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2008-12-17 13:25:47 +0000

    The post_author_and_date partial was no longer required, which in turn makes the postedOn class and formatted_created_date helper redundant.

--- trunk/www/chrisroos/lib/date_time_formatting_helper.rb
+++ trunk/www/chrisroos/lib/date_time_formatting_helper.rb
@@ -1,8 +1,5 @@
 require 'time'
 module DateTimeFormattingHelper
-  def formatted_created_date(page)
-    page.created_at.strftime("%a, %d %b %Y %H:%M:%S")
-  end
   def rfc822_datetime(page)
     page.created_at.rfc822
   end
