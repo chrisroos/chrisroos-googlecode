969ad79a4ab2f9ab8e1b576756178e66f72fc236
Chris Roos <chris@seagul.co.uk>
2008-12-17 13:07:06 +0000

    The use of the post_author_and_date partial on the contents page resulted in two many horizontal rules.  I think this is slightly cleaner.  Note the use of the microformat datetime pattern for the created at date.

--- www/chrisroos/lib/date_time_formatting_helper.rb
+++ www/chrisroos/lib/date_time_formatting_helper.rb
@@ -6,6 +6,10 @@ module DateTimeFormattingHelper
   def rfc822_datetime(page)
     page.created_at.rfc822
   end
+  def short_date(page)
+    date = page.created_at.strftime("%d %b %y")
+    %%<abbr class="createdAt" title="#{page.created_at.xmlschema}">#{date}</abbr>%
+  end
 end
 
 Webby::Helpers.register(DateTimeFormattingHelper)
