215818c3e8875af98974b8322ec391548d9b5bbe
Chris Roos <chris@seagul.co.uk>
2009-02-20 09:02:59 +0000

    Tidying up the markup a little.

--- www/chrisroos/convert_anchors_to_textile_links.rb
+++ www/chrisroos/convert_anchors_to_textile_links.rb
@@ -7,13 +7,13 @@ Dir[File.join(File.dirname(__FILE__), 'content', 'blog', '*.txt')].each do |file
     content.gsub!(r) do |m|
       anchor_attributes, anchor_text = $1, $2
       anchor_href = anchor_attributes[/href="(.*?)"/, 1]
-      # p [m, anchor_href, anchor_text]
-      if anchor_href =~ /^http/ # Let's only replace the http urls for now
-        %%"#{anchor_text}":#{anchor_href}%
-      else
-        m
-      end
+      p [m, anchor_href, anchor_text]
+      # if anchor_href =~ /^http/ # Let's only replace the http urls for now
+      #   %%"#{anchor_text}":#{anchor_href}%
+      # else
+      #   m
+      # end
     end
-    File.open(filename, 'w') { |f| f.puts(content) }
+    # File.open(filename, 'w') { |f| f.puts(content) }
   end
 end
