6d651a962a8f12acadb32e2bcee566b4b6f0f866
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-02-24 23:56:19 +0000

    It might be useful to link to the delicious history of a url from within the list of urls.

--- trunk/scratch/deliciolytics/app/helpers/application_helper.rb
+++ trunk/scratch/deliciolytics/app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
+  def link_to_delicious_url_history(url)
+    link_to h(url.url_hash), "http://delicious.com/url/#{url.url_hash}"
+  end
+  
   def tags_list(tags_and_counts)
     ordered_tags = tags_and_counts.sort_by { |tag, count| count }.reverse.collect { |tag, count| tag }
     h(ordered_tags.join(', '))
