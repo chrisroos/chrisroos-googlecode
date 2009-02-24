5657afe4f56dc0ce3c66bc1428614ade8b412462
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-02-24 23:53:09 +0000

    We can make the top tags look a little prettier.

--- trunk/scratch/deliciolytics/app/helpers/application_helper.rb
+++ trunk/scratch/deliciolytics/app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def tags_list(tags_and_counts)
+    ordered_tags = tags_and_counts.sort_by { |tag, count| count }.reverse.collect { |tag, count| tag }
+    h(ordered_tags.join(', '))
+  end
+  
 end
