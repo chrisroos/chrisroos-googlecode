e6d9314edc54be545e469daa04ce49ed857d7075
Chris Roos <chris@seagul.co.uk>
2009-02-25 22:58:24 +0000

    Another pretty big changeset.
    * I've added a domains/ resource that currently just lists the domains in the database, allowing us to click through to see the urls for those domains.
    * The default root / now just lists all domains as above.
    * I've made the three scripts more generic by requiring you to supply a domain that you wish to work with.
    * The url history for a domain is now stored in bin/bookmarks/<url_hash>.json so that I can store a history of data for testing (even if it's only while I'm developing the site).

--- scratch/deliciolytics/bin/download-sitemap-and-create-urls.rb
+++ scratch/deliciolytics/bin/download-sitemap-and-create-urls.rb
@@ -1,13 +1,15 @@
 require File.join(File.dirname(__FILE__), '..', 'config', 'environment.rb')
 require 'net/http'
 
-domain_url = 'http://chrisroos.local'
+domain_url = ARGV.shift
+raise "You must specify the url of the domain that you wish to download bookmarks for" unless domain_url
+
 sitemap_url = "#{domain_url}/sitemap.xml"
 
 sitemap = Net::HTTP.get(URI.parse(sitemap_url))
 urls = sitemap.scan(/<loc>(.*?)<\/loc>/).flatten
 
-domain = Domain.create!(:domain => domain_url)
+domain = Domain.find_or_create_by_domain(domain_url)
 urls.each do |url|
   domain.urls.create!(:url => url)
 end
--- scratch/deliciolytics/bin/download-url-bookmarks.rb
+++ scratch/deliciolytics/bin/download-url-bookmarks.rb
@@ -3,7 +3,10 @@ require 'md5'
 require 'json'
 require 'net/http'
 
-domain = Domain.find(:first)
+domain_url = ARGV.shift
+raise "You must specify the domain name that you wish to download bookmarks for" unless domain_url
+
+domain = Domain.find_by_domain(domain_url)
 
 bookmarked_urls = domain.urls.collect do |url|
   puts "Collecting URL history for: #{url.url}"
@@ -11,4 +14,6 @@ bookmarked_urls = domain.urls.collect do |url|
   sleep 1
   JSON.parse(json).first
 end.compact
-File.open('bookmarked_urls.json', 'w') { |f| f.puts(bookmarked_urls.to_json) }
+
+bookmark_file = File.join(File.dirname(__FILE__), 'bookmarks', "#{domain.domain_hash}.json")
+File.open(bookmark_file, 'w') { |f| f.puts(bookmarked_urls.to_json) }
