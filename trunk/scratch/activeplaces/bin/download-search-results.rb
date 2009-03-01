98f5acb1dbdc18ea1e82da240531d814c78f3349
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-03-01 18:24:12 +0000

    We now have a script to download search results for a given query.  That query is currently hardcoded but it'll do for now. I've chosen the filename such that we avoid name clashes when downloading multiple search results.  I'm storing the cookie in tmp and have svn:ignored it to keep svn output clean.

---
+++ trunk/scratch/activeplaces/bin/download-search-results.rb
@@ -0,0 +1,14 @@
+require File.join(File.dirname(__FILE__), '..', 'config', 'environment')
+require File.join(File.dirname(__FILE__), 'active_places_helper')
+include ActivePlacesHelper
+
+# We don't care about the output, we just need the cookie
+`curl "http://activeplaces.com/Index_lowgraphic.asp" -c"#{ACTIVE_PLACES_COOKIE_FILE}"`
+
+postcode = 'ct11'
+facility_type = FacilityType.find(7) # swimming pools
+distance = 50
+
+search_result_html = `curl "http://activeplaces.com/FindNearest/SearchResults_LowGraphic.asp?qsPostCode=#{postcode}&qsFacTyp=#{facility_type.id}&qsFacSubTyp=ALL&qsDistance=#{distance}" -b"#{ACTIVE_PLACES_COOKIE_FILE}"`
+search_result_html_file = File.join(Rails.root, 'data', "search-results-#{postcode}-#{facility_type.filename_friendly_name}")
+create_file_with_metadata(search_result_html_file, search_result_html)
