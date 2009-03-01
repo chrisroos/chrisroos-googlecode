186be9454bb4da66caef79269815639a31bd09cd
Chris Roos <chris@seagul.co.uk>
2009-03-01 18:34:37 +0000

    Ooops, I'd accidentally missed the html extension from the search result output.

--- scratch/activeplaces/bin/download-search-results.rb
+++ scratch/activeplaces/bin/download-search-results.rb
@@ -10,5 +10,5 @@ facility_type = FacilityType.find(7) # swimming pools
 distance = 50
 
 search_result_html = `curl "http://activeplaces.com/FindNearest/SearchResults_LowGraphic.asp?qsPostCode=#{postcode}&qsFacTyp=#{facility_type.id}&qsFacSubTyp=ALL&qsDistance=#{distance}" -b"#{ACTIVE_PLACES_COOKIE_FILE}"`
-search_result_html_file = File.join(Rails.root, 'data', "search-results-#{postcode}-#{facility_type.filename_friendly_name}")
+search_result_html_file = File.join(Rails.root, 'data', "search-results-#{postcode}-#{facility_type.filename_friendly_name}.html")
 create_file_with_metadata(search_result_html_file, search_result_html)
