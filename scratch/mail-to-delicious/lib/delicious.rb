f86d2009e7aa356da5e192037baf6a2d417fbeb9
Chris Roos <chris@seagul.co.uk>
2009-03-03 22:21:42 +0000

    We shouldn't explode when no email is supplied.

--- scratch/mail-to-delicious/lib/delicious.rb
+++ scratch/mail-to-delicious/lib/delicious.rb
@@ -9,8 +9,8 @@ module Delicious
     def initialize(email)
       tmail = TMail::Mail.parse(email)
       @title = tmail.subject
-      @body = tmail.parts[0].body # We assume that part 0 is text/plain, this could be somewhat more robust
-      @notes = ''
+      @body = tmail.parts[0].body rescue '' # We assume that part 0 is text/plain, this could be somewhat more robust
+      @notes, @url, @tags = '', '', []
     end
     def parse!
       @body.each_with_index do |line, index|
