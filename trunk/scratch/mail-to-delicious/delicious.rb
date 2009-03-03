ee753d36ae087ff8489e0849cfb247c22a599428
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-03-03 20:28:06 +0000

    Added tag support.  Added a UserAgent to abide by the good-citizen rules.  Oh, and a little refactoring.

--- trunk/scratch/mail-to-delicious/delicious.rb
+++ trunk/scratch/mail-to-delicious/delicious.rb
@@ -1,27 +1,33 @@
 require 'cgi'
 
-# TODO: implement tag (and, possibly shared, support)
-
 class Delicious
   class Bookmark
     Url = 'https://api.del.icio.us/v1/posts/add'
     Username = 'chrisjroos'
     Password = '<top-secret>'
-    def initialize(url, title, notes)
-      @params = {
-        :url => url,
-        :description => title,
-        :extended => notes,
-        :shared => 'no'
-      }
+    UserAgent = "simply delicious ruby wrapper - chrisroos.co.uk"
+    def initialize(url, title, notes, *tags)
+      @url, @title, @notes, @tags = url, title, notes, tags
     end
     def save
-      url = [Url, querystring].join('?')
-      `curl "#{url}" --user "#{basic_auth_credentials}"`
+      `curl "#{api_url}" --user "#{basic_auth_credentials}" --user-agent "#{UserAgent}"`
     end
     private
+      attr_reader :url, :title, :notes
+      def tags
+        @tags.join(' ')
+      end
       def querystring
-        @params.collect { |key, value| [key, sanitize(value)].join('=') }.join('&')
+        params.collect { |key, value| [key, sanitize(value)].join('=') }.join('&')
+      end
+      def params
+        {
+          :url => url,
+          :description => title,
+          :extended => notes,
+          :tags => tags,
+          :shared => 'no'
+        }
       end
       def sanitize(value)
         CGI.escape(value)
@@ -29,7 +35,10 @@ class Delicious
       def basic_auth_credentials
         [Username, Password].join(':')
       end
+      def api_url
+        [Url, querystring].join('?')
+      end
   end
 end
 
-p Delicious::Bookmark.new('http://www.example.com', 'my & unsafe magical :: title', "some\nmulti\nline\nnotes", 'tag1').save
+p Delicious::Bookmark.new('http://www.example.com', 'my & unsafe magical :: title', "some\nmulti\nline\nnotes").save
