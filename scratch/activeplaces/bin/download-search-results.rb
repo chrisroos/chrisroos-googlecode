ce49087a3803d1c3e862a80e5e55f882fc18871b
Chris Roos <chris@seagul.co.uk>
2009-03-06 08:25:55 +0000

    My original plan was to search for each facility type within each outcode.  I think that's probably overkill so I'm just going to search for all facilities within a 50 mile radius of each outcode.  I'm hoping that should be sufficient (with the overlapping postcodes) to get data of all facilities in their database.

--- scratch/activeplaces/bin/download-search-results.rb
+++ scratch/activeplaces/bin/download-search-results.rb
@@ -3,12 +3,11 @@ require File.join(File.dirname(__FILE__), 'active_places_helper')
 include ActivePlacesHelper
 
 # We don't care about the output, we just need the cookie
-`curl "http://activeplaces.com/Index_lowgraphic.asp" -c"#{ACTIVE_PLACES_COOKIE_FILE}"`
+`curl "http://activeplaces.com/Index_lowgraphic.asp" -c"#{ACTIVE_PLACES_COOKIE_FILE}" -A"http://chrisroos.co.uk"`
 
 postcode = 'ct11'
-facility_type = FacilityType.find(7) # swimming pools
 distance = 50
 
-search_result_html = `curl "http://activeplaces.com/FindNearest/SearchResults_LowGraphic.asp?qsPostCode=#{postcode}&qsFacTyp=#{facility_type.id}&qsFacSubTyp=ALL&qsDistance=#{distance}" -b"#{ACTIVE_PLACES_COOKIE_FILE}"`
-search_result_html_file = File.join(Rails.root, 'data', "search-results-#{postcode}-#{facility_type.filename_friendly_name}.html")
+search_result_html = `curl "http://activeplaces.com/FindNearest/SearchResults_LowGraphic.asp?qsPostCode=#{postcode}&qsFacTyp=ALL&qsFacSubTyp=ALL&qsDistance=#{distance}" -b"#{ACTIVE_PLACES_COOKIE_FILE}"`
+search_result_html_file = File.join(Rails.root, 'data', "search-results-#{postcode}-all.html")
 create_file_with_metadata(search_result_html_file, search_result_html)
