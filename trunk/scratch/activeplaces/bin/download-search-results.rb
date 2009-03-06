42ffcdb5034ec9f5a7d2afd450100e2e2cbc43c9
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-03-06 21:28:16 +0000

    We can re-use our curl wrapper and get rid of the cookie check.  We can also make use of the SEARCH_RESULTS_DIRECTORY constant and, to play fair, give the server a little more breathing room between requests.

--- trunk/scratch/activeplaces/bin/download-search-results.rb
+++ trunk/scratch/activeplaces/bin/download-search-results.rb
@@ -2,9 +2,6 @@ require File.join(File.dirname(__FILE__), '..', 'config', 'environment')
 require File.join(File.dirname(__FILE__), 'active_places_helper')
 include ActivePlacesHelper
 
-# We don't care about the output, we just need the cookie
-`curl "http://activeplaces.com/Index_lowgraphic.asp" -c"#{ACTIVE_PLACES_COOKIE_FILE}" -A"http://chrisroos.co.uk" -s`
-
 postcodes = []
 File.open(File.join(Rails.root, 'data', 'uk-postcode-outcodes.csv')) do |file|
   file.each_with_index do |line, index|
@@ -15,14 +12,13 @@ end
 
 puts "Started: #{Time.now}"
 postcodes.each_with_index do |postcode, index|
-  puts "Postcode (#{index}): #{postcode}"
+  puts "#{Time.now}.  Postcode (#{index}): #{postcode}"
   distance = 50
   
-  cmd = %%curl "http://activeplaces.com/FindNearest/SearchResults_LowGraphic.asp?qsPostCode=#{postcode}&qsFacTyp=ALL&qsFacSubTyp=ALL&qsDistance=#{distance}" -b"#{ACTIVE_PLACES_COOKIE_FILE}" -A"http://chrisroos.co.uk" -s%
-  search_result_html = `#{cmd}`
-  search_result_html_file = File.join(Rails.root, 'data', "search-results-#{postcode}-all.html")
+  search_result_html = curl("http://activeplaces.com/FindNearest/SearchResults_LowGraphic.asp?qsPostCode=#{postcode}&qsFacTyp=ALL&qsFacSubTyp=ALL&qsDistance=#{distance}")
+  search_result_html_file = File.join(SEARCH_RESULTS_DIRECTORY, "search-results-#{postcode}-all.html")
   create_file_with_metadata(search_result_html_file, search_result_html)
   
-  sleep 1 # Give the server a chance to stay up
+  sleep 2 # Give the server a chance to stay up
 end
 puts "Finished: #{Time.now}"
