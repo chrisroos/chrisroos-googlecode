44c33991dec571afb41871513fb7a9a9e80a7de7
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-03-28 06:46:50 +0000

    This was the web service hack written at rewired state.  My original intention was to update public-swimming-pools.co.uk which is why that name was in the config file.  Now changed to better reflect what it actually is.

---
+++ trunk/scratch/activeplaces-web-service/config/deploy.rb
@@ -0,0 +1,15 @@
+set :application, "activeplaces-web-service"
+set :repository,  "http://chrisroos.googlecode.com/svn/trunk/scratch/activeplaces-web-service"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/home/chrisroos/apps/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+
+role :app, "seagul.co.uk"
+role :web, "seagul.co.uk"
+role :db,  "seagul.co.uk", :primary => true
