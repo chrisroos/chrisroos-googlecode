4d8f080eedc4aea456d88a0959e1ad1060f2d973
Chris Roos <chris@seagul.co.uk>
2009-03-30 09:17:31 +0000

    Make the code a little more robust (checking for the presence of stuff before using it) and remove the hardcoded use of the id property.

--- scratch/javascript-permalinks/permalinks.js
+++ scratch/javascript-permalinks/permalinks.js
@@ -1,6 +1,9 @@
+// TODO
+// * Deal with multiple keys
+
 // The rule
-var urlRegex = /http:\/\/www.theyworkforyou.com\/wrans/
-var wanted_keys = 'id';
+var urlRegex = /http:\/\/localhost/
+var wanted_key = 'id';
 
 if (urlRegex.test(document.location.href)) {
 
@@ -8,15 +11,22 @@ if (urlRegex.test(document.location.href)) {
   var url = document.location;
   var queryString = url.search.replace(/^\?/, ''); // Remove leading question mark
   var key_value_pairs = queryString.split('&');
-  var keys_and_values;
-  for (var i = 0; i < key_value_pairs.length; i++) {
-    var key = key_value_pairs[i].split('=')[0];
-    var value = key_value_pairs[i].split('=')[1];
-    keys_and_values[key] = value;
+  if (key_value_pairs.length > 0 && key_value_pairs[0] != '') {
+    var keys_and_values = {};
+    for (var i = 0; i < key_value_pairs.length; i++) {
+      var key = key_value_pairs[i].split('=')[0];
+      var value = key_value_pairs[i].split('=')[1];
+      keys_and_values[key] = value;
+    }
   }
   
   // Generate the permalink
-  var permalink = url.protocol + '//' + url.host + url.pathname + '?id=' + keys_and_values.id;
-  document.location.replace(permalink);
-
+  if (keys_and_values && keys_and_values[wanted_key]) {
+    var queryString = [wanted_key, keys_and_values[wanted_key]].join('=')
+    var permalink = url.protocol + '//' + url.host + url.pathname + '?' + queryString;
+    console.log('Permalink for ' + url + ' is ' + permalink);
+    // document.location.replace(permalink);
+  } else {
+    console.log('Permalink could not be generated for ' + url);
+  }
 }
