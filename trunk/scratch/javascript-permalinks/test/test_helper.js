b1cd052cba7a115d7e6c3b57bbe1cb524106a67e
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-04-07 22:44:02 +0000

    Changing the test_helper allows me to ensure that I'm inserting the link rel=canonical into the head of the document.

--- trunk/scratch/javascript-permalinks/test/test_helper.js
+++ trunk/scratch/javascript-permalinks/test/test_helper.js
@@ -1,6 +1,7 @@
 TestHelper = {  
   getCanonicalLinkElements: function() {
-    var linkElements = document.getElementsByTagName('link');
+    var head = document.getElementsByTagName('head')[0];
+    var linkElements = head.getElementsByTagName('link');
     var canonicalLinkElements = [];
     for (var i = 0; i < linkElements.length; i++) {
       if (linkElements[i].getAttribute('rel') == 'canonical') {
