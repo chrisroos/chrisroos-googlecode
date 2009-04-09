5855d3e543e9b78e3320f6fba7927ac5c7cd1e46
Chris Roos <chris@seagul.co.uk>
2009-04-09 08:12:37 +0000

    Rearranging the code so that similar things are close to each other.

--- scratch/javascript-permalinks/canonical-url.user.js
+++ scratch/javascript-permalinks/canonical-url.user.js
@@ -7,6 +7,9 @@
 
 CanonicalUrl = {}
 
+// **********************************************
+// CanonicalUrl members
+
 CanonicalUrl.queryString = function(location) {
   var keysAndValues = {};
   if (m = location.href.match(/\?(.*)/)) {
@@ -39,11 +42,6 @@ CanonicalUrl.Url = function(location) {
 CanonicalUrl.Permalink = function(location) {
   this.location = location;
 }
-CanonicalUrl.Permalink.prototype.href = function() {
-  var permalink = CanonicalUrl.Rules.apply(this.location);
-  if (!permalink) { permalink = ''; }
-  return permalink;
-}
 
 CanonicalUrl.requiredKeyRule = function(url, key) {
   if (key && url.queryString && url.queryString[key]) {
@@ -58,39 +56,10 @@ CanonicalUrl.Rule = function(urlPattern, modifier) {
   this.urlPattern = urlPattern;
   this.modifier = modifier;
 };
+
 CanonicalUrl.RuleCollection = function() {
   this.rules = [];
 }
-CanonicalUrl.RuleCollection.prototype.add = function(urlPattern, modifier) {
-  var rule = new CanonicalUrl.Rule(urlPattern, modifier);
-  this.rules.push(rule);
-};
-CanonicalUrl.RuleCollection.prototype.apply = function(location) {
-  for (var i = 0; i < this.rules.length; i++) {
-    var rule = this.rules[i];
-    if (rule.urlPattern.test(location.href)) {
-      if (rule.modifier && typeof(rule.modifier) == 'function')
-        return rule.modifier(new CanonicalUrl.Url(location));
-    }
-  }
-}
-CanonicalUrl.Rules = new CanonicalUrl.RuleCollection();
-CanonicalUrl.Rules.add(/userscript_integration_test\.html/, function(url) { 
-  return 'userscript-permalink';
-});
-CanonicalUrl.Rules.add(/google\.co\.uk\/search/, function(url) { 
-  return CanonicalUrl.requiredKeyRule(url, 'q');
-});
-CanonicalUrl.Rules.add(/theyworkforyou\.com\/wrans/, function(url) { 
-  return CanonicalUrl.requiredKeyRule(url, 'id');
-});
-CanonicalUrl.Rules.add(/cgi\.ebay\.co\.uk/, function(url) {
-  var hash = url.queryString.hash;
-  if (m = hash.match(/item(\d+)/)) {
-    var itemId = m[1];
-    return 'http://cgi.ebay.co.uk/ws/eBayISAPI.dll?ViewItem&item=' + itemId;
-  }
-});
 
 CanonicalUrl.CanonicalLink = {
   'write' : function(permalink) {
@@ -109,4 +78,51 @@ CanonicalUrl.insert = function(location) {
   CanonicalUrl.CanonicalLink.write(permalink);
 }
 
+// **********************************************
+// CanonicalUrl member prototypes
+
+CanonicalUrl.Permalink.prototype.href = function() {
+  var permalink = CanonicalUrl.Rules.apply(this.location);
+  if (!permalink) { permalink = ''; }
+  return permalink;
+}
+
+CanonicalUrl.RuleCollection.prototype.add = function(urlPattern, modifier) {
+  var rule = new CanonicalUrl.Rule(urlPattern, modifier);
+  this.rules.push(rule);
+};
+
+CanonicalUrl.RuleCollection.prototype.apply = function(location) {
+  for (var i = 0; i < this.rules.length; i++) {
+    var rule = this.rules[i];
+    if (rule.urlPattern.test(location.href)) {
+      if (rule.modifier && typeof(rule.modifier) == 'function')
+        return rule.modifier(new CanonicalUrl.Url(location));
+    }
+  }
+}
+
+// **********************************************
+// The actual real-life rules
+
+CanonicalUrl.Rules = new CanonicalUrl.RuleCollection();
+CanonicalUrl.Rules.add(/userscript_integration_test\.html/, function(url) { 
+  return 'userscript-permalink';
+});
+CanonicalUrl.Rules.add(/google\.co\.uk\/search/, function(url) { 
+  return CanonicalUrl.requiredKeyRule(url, 'q');
+});
+CanonicalUrl.Rules.add(/theyworkforyou\.com\/wrans/, function(url) { 
+  return CanonicalUrl.requiredKeyRule(url, 'id');
+});
+CanonicalUrl.Rules.add(/cgi\.ebay\.co\.uk/, function(url) {
+  var hash = url.queryString.hash;
+  if (m = hash.match(/item(\d+)/)) {
+    var itemId = m[1];
+    return 'http://cgi.ebay.co.uk/ws/eBayISAPI.dll?ViewItem&item=' + itemId;
+  }
+});
+
+// **********************************************
+// Do the magic
 CanonicalUrl.insert(document.location);
