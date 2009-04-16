646bcff4b15f2769cc03f1e4cccb53bf107b028f
Chris Roos <chris@seagul.co.uk>
2009-04-16 13:36:04 +0000

    Spiking on a greasemonkey script that highlights parts of a document that I can navigate to.

---
+++ scratch/greasemonkey-highlight-named-anchors/highlight-named-anchors.user.js
@@ -0,0 +1,19 @@
+var interestingNodes = ['a', 'p', 'div'];
+
+for (var nodeIndex = 0; nodeIndex < interestingNodes.length; nodeIndex++) {
+  var nodeName = interestingNodes[nodeIndex];
+  
+  var elements = Array.slice(document.getElementsByTagName(nodeName)); // We slice in order to convert the 'live' NodeList to a static array.
+  for (var elementIndex = 0; elementIndex < elements.length; elementIndex++) {
+    var element = elements[elementIndex];
+    
+    var target = element.getAttribute('name') || element.getAttribute('id');
+    if (target) {
+      var anchor = document.createElement('a');
+      anchor.setAttribute('href', '#' + target);
+      anchor.setAttribute('style', 'color: #f00; background-color: #ff0;')
+      anchor.appendChild(document.createTextNode('#'));
+      element.parentNode.insertBefore(anchor, element);
+    }
+  }
+}
