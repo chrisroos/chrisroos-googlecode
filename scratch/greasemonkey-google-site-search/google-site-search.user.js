f8249e4903ccf1c43097f52d205c70affc613b50
Chris Roos <chris@seagul.co.uk>
2009-04-17 11:00:38 +0000

    Ah, so this is the magical syntax I needed to remove the site search box from google itself.

--- scratch/greasemonkey-google-site-search/google-site-search.user.js
+++ scratch/greasemonkey-google-site-search/google-site-search.user.js
@@ -3,8 +3,8 @@
 // @namespace     http://chrisroos.co.uk/
 // @description   Insert a 'google site search' search form to every page.
 // @include       *
-// @exclude       www.google.co.uk
-// @exclude       www.google.com
+// @exclude       http://www.google.co.uk*
+// @exclude       http://www.google.com*
 // ==/UserScript==
 
 (function() {
