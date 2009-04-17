31a712b323141bf21d46661a7dec235d93a35f73
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-04-17 10:51:10 +0000

    Protect the outside world from my javascript goodness.

--- trunk/scratch/greasemonkey-highlight-named-anchors/highlight-named-anchors.user.js
+++ trunk/scratch/greasemonkey-highlight-named-anchors/highlight-named-anchors.user.js
@@ -5,22 +5,24 @@
 // @include       *
 // ==/UserScript==
 
-var interestingNodes = ['a'];
+(function() {
+  var interestingNodes = ['a'];
 
-for (var nodeIndex = 0; nodeIndex < interestingNodes.length; nodeIndex++) {
-  var nodeName = interestingNodes[nodeIndex];
+  for (var nodeIndex = 0; nodeIndex < interestingNodes.length; nodeIndex++) {
+    var nodeName = interestingNodes[nodeIndex];
   
-  var elements = Array.slice(document.getElementsByTagName(nodeName)); // We slice in order to convert the 'live' NodeList to a static array.
-  for (var elementIndex = 0; elementIndex < elements.length; elementIndex++) {
-    var element = elements[elementIndex];
+    var elements = Array.slice(document.getElementsByTagName(nodeName)); // We slice in order to convert the 'live' NodeList to a static array.
+    for (var elementIndex = 0; elementIndex < elements.length; elementIndex++) {
+      var element = elements[elementIndex];
     
-    var target = element.getAttribute('name') || element.getAttribute('id');
-    if (target) {
-      var anchor = document.createElement('a');
-      anchor.setAttribute('href', '#' + target);
-      anchor.setAttribute('style', 'color: #f00; background-color: #ff0;')
-      anchor.appendChild(document.createTextNode('#'));
-      element.parentNode.insertBefore(anchor, element);
+      var target = element.getAttribute('name') || element.getAttribute('id');
+      if (target) {
+        var anchor = document.createElement('a');
+        anchor.setAttribute('href', '#' + target);
+        anchor.setAttribute('style', 'color: #f00; background-color: #ff0;')
+        anchor.appendChild(document.createTextNode('#'));
+        element.parentNode.insertBefore(anchor, element);
+      }
     }
   }
-}
+})()
