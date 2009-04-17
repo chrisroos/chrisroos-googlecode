2600a691a6ccbd66acb56a5352d5fa1e0d3bd1a0
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-04-17 09:39:39 +0000

    A spike on adding a google site search box to every page you visit.

---
+++ trunk/scratch/greasemonkey-google-site-search/google-site-search.user.js
@@ -0,0 +1,30 @@
+(function() {
+  var s1 = document.createElement('input');
+  s1.setAttribute('type', 'hidden');
+  s1.setAttribute('name', 'q');
+  s1.setAttribute('value', 'site:' + window.location.host); // insert the domain of the site we're visiting here
+
+  var s2 = document.createElement('input');
+  s2.setAttribute('type', 'text');
+  s2.setAttribute('name', 'q');
+  s2.setAttribute('accesskey', '9');
+
+  var s3 = document.createElement('input');
+  s3.setAttribute('type', 'submit');
+  s3.setAttribute('name', 'sa'); // Is this name important?
+  s3.setAttribute('value', 'search');
+
+  var f = document.createElement('form');
+  f.setAttribute('action', 'http://www.google.co.uk/search');
+
+  f.appendChild(s1);
+  f.appendChild(s2);
+  f.appendChild(s3);
+
+  var d = document.createElement('div');
+  d.setAttribute('style', 'width: 100%; padding: 5px; text-align: right');
+
+  d.appendChild(f);
+
+  document.body.insertBefore(d, document.body.firstChild);
+})()
