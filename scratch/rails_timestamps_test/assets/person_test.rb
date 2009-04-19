369b613b0e15d0ee39a0b6ed2af116c2637d2b61
Chris Roos <chris@seagul.co.uk>
2009-04-19 08:13:45 +0000

    I'd accidentally left the person_test in an inconsistent state (the tests weren't all identical, which they should be).

--- scratch/rails_timestamps_test/assets/person_test.rb
+++ scratch/rails_timestamps_test/assets/person_test.rb
@@ -9,26 +9,40 @@ class PersonTest < Test::Unit::TestCase
   
   def test_should_be_able_to_set_created_on
     person = Person.new(:created_on => @date)
-    assert_equal @date, person.created_on, "FAIL: Couldn't mass assign the created_on attribute"
+    assert_equal @date, person.created_on, assignment_failure_message(:created_on)
     person.save!
-    assert_equal @date, person.created_on, "FAIL: Couldn't mass assign the created_on attribute"
+    assert_equal @date, person.created_on, persistance_failure_message(:created_on)
   end
   
   def test_should_be_able_to_set_created_at
-    person = Person.create!(:created_at => @timestamp)
-    assert_equal @timestamp, person.created_at, "FAIL: Couldn't mass assign the created_at attribute"
+    person = Person.new(:created_at => @timestamp)
+    assert_equal @timestamp, person.created_at, assignment_failure_message(:created_at)
+    person.save!
+    assert_equal @timestamp, person.created_at, persistance_failure_message(:created_at)
   end
   
   def test_should_be_able_to_set_updated_on
-    person = Person.new(:updated_on => @date, :created_at => @timestamp, :created_on => @date, :updated_at => @timestamp)
-    assert_equal @date, person.updated_on, "FAIL: Couldn't mass assign the updated_on attribute"
+    person = Person.new(:updated_on => @date)
+    assert_equal @date, person.updated_on, assignment_failure_message(:updated_on)
     person.save!
-    assert_equal @date, person.updated_on, "FAIL: Couldn't mass assign the updated_on attribute"
+    assert_equal @date, person.updated_on, persistance_failure_message(:updated_on)
   end
   
   def test_should_be_able_to_set_updated_at
-    person = Person.create!(:updated_at => @timestamp)
-    assert_equal @timestamp, person.updated_at, "FAIL: Couldn't mass assign the updated_at attribute"
+    person = Person.new(:updated_at => @timestamp)
+    assert_equal @timestamp, person.updated_at, assignment_failure_message(:updated_at)
+    person.save!
+    assert_equal @timestamp, person.updated_at, persistance_failure_message(:updated_at)
   end
+  
+  private
+  
+    def assignment_failure_message(column)
+      "FAIL: Couldn't mass assign the #{column} attribute"
+    end
+    
+    def persistance_failure_message(column)
+      "FAIL: Couldn't persist the mass assigned #{column} attribute"
+    end
   
 end
