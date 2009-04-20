700c21f67060783318b90a3d927d94a292e5b36d
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-04-20 09:36:22 +0000

    Rather than hardcode the site:xxx parameter, we allow the user to select from a list of domain or domain + paths.  I've also renamed the search button to make it more obvious that this is my extension (I had been confused a couple of times by the inserted search form looking like an actual search form).

--- trunk/scratch/greasemonkey-google-site-search/google-site-search.user.js
+++ trunk/scratch/greasemonkey-google-site-search/google-site-search.user.js
@@ -8,10 +8,40 @@
 // ==/UserScript==
 
 (function() {
-  var s1 = document.createElement('input');
-  s1.setAttribute('type', 'hidden');
-  s1.setAttribute('name', 'q');
-  s1.setAttribute('value', 'site:' + window.location.host); // insert the domain of the site we're visiting here
+  // Create an array of 'sites' (either the domain or domain + paths) that the user can use to search google with
+  var sites = [window.location.host];
+  if (window.location.pathname != '/') {
+    // pathComponentsTemp will contain empty elements.  We iterate over this and populate pathComponents with just the non-empty elements
+    var pathComponentsTemp = window.location.pathname.split('/');
+    var pathComponents = [];
+    for (var pathComponentIndex = 0; pathComponentIndex < pathComponentsTemp.length; pathComponentIndex++) {
+      if (pathComponentsTemp[pathComponentIndex]) {
+        pathComponents.push(pathComponentsTemp[pathComponentIndex]);
+      }
+    }
+    // We can't use pathComponents.length in the for (...) statement because it changes as we pop() elements
+    var pathComponentCount = pathComponents.length;
+    for (var pathComponentIndex = 0; pathComponentIndex < pathComponentCount; pathComponentIndex++) {
+      sites.push(window.location.host + '/' + pathComponents.join('/'));
+      pathComponents.pop();
+    }
+  }
+  
+  var sitesSelect = document.createElement('select');
+  sitesSelect.setAttribute('name', 'q');
+  // TODO: This isn't working when the script is installed as a greasemonkey extension
+  // sitesSelect.onkeypress = function(event) {
+  //   if (event.keyCode == 13)
+  //     this.parentNode.submit();
+  // };
+  sites.sort(); // This should result in a list of sites ordered by length
+   for (var i = 0; i < sites.length; i++) {
+     var site = sites[i];
+     var sitesOption = document.createElement('option');
+     sitesOption.setAttribute('value', 'site:' + site);
+     sitesOption.appendChild(document.createTextNode(site));
+     sitesSelect.appendChild(sitesOption);
+   }
 
   var s2 = document.createElement('input');
   s2.setAttribute('type', 'text');
@@ -21,13 +51,13 @@
   var s3 = document.createElement('input');
   s3.setAttribute('type', 'submit');
   s3.setAttribute('name', 'sa'); // Is this name important?
-  s3.setAttribute('value', 'search');
+  s3.setAttribute('value', 'google site search');
 
   var f = document.createElement('form');
   f.setAttribute('action', 'http://www.google.co.uk/search');
 
-  f.appendChild(s1);
   f.appendChild(s2);
+  f.appendChild(sitesSelect);
   f.appendChild(s3);
 
   var d = document.createElement('div');
