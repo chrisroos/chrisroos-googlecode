d15f9ff4d199c3927babb2c9c21ef3721aaf9753
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-04-27 13:19:03 +0000

    Removing all the code that deals with 'deploying' this site as delicious-permalinks.seagul.co.uk now just redirects to the extension hosted at addons.mozilla.org, and the apache config is currently in scratch/apache-config-files.

--- trunk/delicious-permalinks/config/deploy.rb
+++
@@ -1,9 +0,0 @@
-set :application, "delicious-permalinks"
-set :repository,  "http://chrisroos.googlecode.com/svn/trunk/#{application}"
-
-set :deploy_to, "/home/chrisroos/www/#{application}.seagul.co.uk"
-set :deploy_via, :copy
-
-role :app, "jail0093.vps.exonetric.net"
-role :web, "jail0093.vps.exonetric.net"
-role :db,  "jail0093.vps.exonetric.net", :primary => true
