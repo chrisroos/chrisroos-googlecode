c48a3513eac0568daabecc58da62381533aa8c5c
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-04-28 21:50:40 +0000

    This only contained a skeleton apache config file.  I've moved that to repos/scratch/apache-config-files so this can now be removed.

--- trunk/www/seagul.co.uk/config/deploy.rb
+++
@@ -1,9 +0,0 @@
-set :application, "seagul.co.uk"
-set :repository,  "http://chrisroos.googlecode.com/svn/trunk/www/seagul.co.uk"
-
-set :deploy_to, "/home/chrisroos/www/#{application}"
-set :deploy_via, :copy
-
-role :app, "jail0093.vps.exonetric.net"
-role :web, "jail0093.vps.exonetric.net"
-role :db,  "jail0093.vps.exonetric.net", :primary => true
