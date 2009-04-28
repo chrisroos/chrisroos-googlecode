5ba857f2e5ce90d641aa6d5803cd3ab6522b2773
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-04-28 22:12:11 +0000

    I've moved the apache config for apceastkent.co.uk to scratch/apache-config-files so we can remove this partial config file from here.

--- trunk/www/apceastkent.co.uk/config/deploy.rb
+++
@@ -1,9 +0,0 @@
-set :application, "apceastkent.co.uk"
-set :repository,  "http://chrisroos.googlecode.com/svn/trunk/apc_website"
-
-set :deploy_to, "/home/chrisroos/www/#{application}"
-set :deploy_via, :copy
-
-role :app, "jail0093.vps.exonetric.net"
-role :web, "jail0093.vps.exonetric.net"
-role :db,  "jail0093.vps.exonetric.net", :primary => true
