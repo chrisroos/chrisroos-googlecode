5a5bb2dd277dd7ac615b4cff668c66e0633d4ee8
Chris Roos <chris@seagul.co.uk>
2009-05-05 21:00:59 +0000

    I'm expecting this helper to be svn:externalised into a vendor directory within the extension itself.

--- scratch/firefox-extension-helper/firefox-extension.rb
+++ scratch/firefox-extension-helper/firefox-extension.rb
@@ -6,7 +6,7 @@ require File.join(File.dirname(__FILE__), 'lib', 'install_manifest')
 require File.join(File.dirname(__FILE__), 'lib', 'update_manifest')
 
 module FirefoxExtension
-  EXTENSION_ROOT             = File.expand_path(File.join(File.dirname(__FILE__)))
+  EXTENSION_ROOT             = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
   EXTENSION_BUILD_DIRECTORY  = File.join(EXTENSION_ROOT, 'build')
   EXTENSION_CONFIG_DIRECTORY = File.join(EXTENSION_ROOT, 'config')
   EXTENSION_SRC_DIRECTORY    = File.join(EXTENSION_ROOT, 'src')
