998261e1fd3813726f87611a44fc2ff0ba0a0ae9
Chris Roos <chris@seagul.co.uk>
2009-05-05 20:52:03 +0000

    Namespacing the Firefox extension magic just in case I need to play nice with any other libraries.

--- firefox-extensions/delicious-permalinks/firefox-extension.rb
+++ firefox-extensions/delicious-permalinks/firefox-extension.rb
@@ -1,7 +1,9 @@
 require File.join(File.dirname(__FILE__), 'lib', 'install_manifest')
 require File.join(File.dirname(__FILE__), 'lib', 'update_manifest')
 
-EXTENSION_ROOT             = File.expand_path(File.join(File.dirname(__FILE__)))
-EXTENSION_BUILD_DIRECTORY  = File.join(EXTENSION_ROOT, 'build')
-EXTENSION_CONFIG_DIRECTORY = File.join(EXTENSION_ROOT, 'config')
-EXTENSION_SRC_DIRECTORY    = File.join(EXTENSION_ROOT, 'src')
+module FirefoxExtension
+  EXTENSION_ROOT             = File.expand_path(File.join(File.dirname(__FILE__)))
+  EXTENSION_BUILD_DIRECTORY  = File.join(EXTENSION_ROOT, 'build')
+  EXTENSION_CONFIG_DIRECTORY = File.join(EXTENSION_ROOT, 'config')
+  EXTENSION_SRC_DIRECTORY    = File.join(EXTENSION_ROOT, 'src')
+end
