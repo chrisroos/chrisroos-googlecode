6fcdf4be6da91c62bef8cbaaec47a5f95de0d236
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-05-05 20:52:03 +0000

    Namespacing the Firefox extension magic just in case I need to play nice with any other libraries.

--- trunk/firefox-extensions/delicious-permalinks/lib/update_manifest.rb
+++ trunk/firefox-extensions/delicious-permalinks/lib/update_manifest.rb
@@ -2,25 +2,27 @@ require 'rubygems'
 require 'builder'
 require 'yaml'
 
-class UpdateManifest
-  def self.generate(configuration, output_directory)
-    buffer = StringIO.new
+module FirefoxExtension
+  class UpdateManifest
+    def self.generate(configuration, output_directory)
+      buffer = StringIO.new
 
-    builder = Builder::XmlMarkup.new(:target=>buffer, :indent=>2)
-    builder.instruct!
-    builder.r(:RDF, 'xmlns:r' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#', :xmlns => 'http://www.mozilla.org/2004/em-rdf#') do
-      builder.r(:Description, :about => "urn:mozilla:extension:#{configuration[:extension][:id]}") do
-        builder.updates do
-          builder.r(:Seq) do
-            builder.r(:li) do
-              builder.r(:Description) do
-                builder.version(configuration[:extension][:version])
-                builder.targetApplication do
-                  builder.r(:Description) do
-                    builder.id(configuration[:target_application][:id])
-                    builder.minVersion(configuration[:target_application][:min_version])
-                    builder.maxVersion(configuration[:target_application][:max_version])
-                    builder.updateLink(configuration[:extension][:install_url])
+      builder = Builder::XmlMarkup.new(:target=>buffer, :indent=>2)
+      builder.instruct!
+      builder.r(:RDF, 'xmlns:r' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#', :xmlns => 'http://www.mozilla.org/2004/em-rdf#') do
+        builder.r(:Description, :about => "urn:mozilla:extension:#{configuration[:extension][:id]}") do
+          builder.updates do
+            builder.r(:Seq) do
+              builder.r(:li) do
+                builder.r(:Description) do
+                  builder.version(configuration[:extension][:version])
+                  builder.targetApplication do
+                    builder.r(:Description) do
+                      builder.id(configuration[:target_application][:id])
+                      builder.minVersion(configuration[:target_application][:min_version])
+                      builder.maxVersion(configuration[:target_application][:max_version])
+                      builder.updateLink(configuration[:extension][:install_url])
+                    end
                   end
                 end
               end
@@ -28,9 +30,9 @@ class UpdateManifest
           end
         end
       end
-    end
 
-    output_file = File.join(output_directory, 'update.rdf')
-    File.open(output_file, 'w') { |f| f.puts(buffer.string) }
+      output_file = File.join(output_directory, 'update.rdf')
+      File.open(output_file, 'w') { |f| f.puts(buffer.string) }
+    end
   end
 end
