e56196f1a5ab09f31e50f19c0beff724d7a9199f
Chris Roos <chris@seagul.co.uk>
2009-05-12 22:39:17 +0000

    I've created a symlink on the server so that /u/apps points to /home/chrisroos/apps.  This will allow me to update all capistrano recipes to use the default deploy_to path.

--- scratch/activeplaces-web-service/config/deploy.rb
+++ scratch/activeplaces-web-service/config/deploy.rb
@@ -1,10 +1,9 @@
 set :application, "activeplaces-web-service"
-set :repository,  "http://chrisroos.googlecode.com/svn/trunk/scratch/activeplaces-web-service"
+set :repository,  "http://chrisroos.googlecode.com/svn/trunk/scratch/#{application}"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/home/chrisroos/apps/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
