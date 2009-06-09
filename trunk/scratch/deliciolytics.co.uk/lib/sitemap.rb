0c962ccf7be0474cae3a194c28b930cfe7d1157d
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-06-09 20:28:27 +0000

    The domain should always end with a slash which means the slash here causes problems (like 404 problems).

--- trunk/scratch/deliciolytics.co.uk/lib/sitemap.rb
+++ trunk/scratch/deliciolytics.co.uk/lib/sitemap.rb
@@ -2,7 +2,7 @@ require 'net/http'
 
 class Sitemap
   def initialize(domain)
-    @sitemap_url = "#{domain}/sitemap.xml"
+    @sitemap_url = "#{domain}sitemap.xml"
   end
   def urls
     @urls ||= sitemap.scan(/<loc>(.*?)<\/loc>/).flatten
