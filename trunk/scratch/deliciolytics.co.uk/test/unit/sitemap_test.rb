41df189503187f570bfe0ce65f6aba65f12cdaf4
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-06-17 20:26:17 +0000

    James Andrews has a sitemap index file that contains links to multiple sitemaps.  I've modified the code slightly to deal with multiple sitemaps (previously I was only dealing with one).

--- trunk/scratch/deliciolytics.co.uk/test/unit/sitemap_test.rb
+++ trunk/scratch/deliciolytics.co.uk/test/unit/sitemap_test.rb
@@ -3,8 +3,8 @@ require 'fakeweb'
 
 class SitemapTest < Test::Unit::TestCase
   
-  def test_should_parse_the_urls_from_the_sitemap
-    FakeWeb.register_uri(:get, "http://example.com/sitemap.xml", :string => sitemap)
+  def test_should_parse_the_urls_from_a_sitemap_in_the_expected_location
+    FakeWeb.register_uri(:get, "http://example.com/sitemap.xml", :string => sitemap1)
     
     sitemap = Sitemap.new('http://example.com/')
     
@@ -12,19 +12,32 @@ class SitemapTest < Test::Unit::TestCase
     assert sitemap.urls.include?('http://example.com/resource-2')
   end
   
-  def test_should_parse_the_urls_from_the_sitemap_indicated_in_the_sitemap_index
+  def test_should_parse_the_urls_from_a_sitemap_linked_from_the_sitemap_index
     FakeWeb.register_uri(:get, "http://example.com/sitemap.xml", :string => sitemap_index)
-    FakeWeb.register_uri(:get, "http://example.com/path/sitemap.xml", :string => sitemap)
+    FakeWeb.register_uri(:get, "http://example.com/sitemap1.xml", :string => sitemap1)
     
     sitemap = Sitemap.new('http://example.com/')
     
     assert sitemap.urls.include?('http://example.com/resource-1')
     assert sitemap.urls.include?('http://example.com/resource-2')
+  end
+  
+  def test_should_parse_the_urls_from_multiple_sitemaps_linked_from_the_sitemap_index
+    FakeWeb.register_uri(:get, "http://example.com/sitemap.xml", :string => sitemap_index)
+    FakeWeb.register_uri(:get, "http://example.com/sitemap1.xml", :string => sitemap1)
+    FakeWeb.register_uri(:get, "http://example.com/sitemap2.xml", :string => sitemap2)
+    
+    sitemap = Sitemap.new('http://example.com/')
+    
+    assert sitemap.urls.include?('http://example.com/resource-1')
+    assert sitemap.urls.include?('http://example.com/resource-2')
+    assert sitemap.urls.include?('http://example.com/resource-100')
+    assert sitemap.urls.include?('http://example.com/resource-101')
   end
   
 private
   
-  def sitemap
+  def sitemap1
 <<-EndSitemap
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -34,12 +47,23 @@ private
 EndSitemap
   end
   
+  def sitemap2
+<<-EndSitemap
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url><loc>http://example.com/resource-100</loc></url>
+  <url><loc>http://example.com/resource-101</loc></url>
+</urlset>
+EndSitemap
+  end
+  
   def sitemap_index
 <<-EndSitemapIndex
 <?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
-    <loc>http://example.com/path/sitemap.xml</loc>
+    <loc>http://example.com/sitemap1.xml</loc>
+    <loc>http://example.com/sitemap2.xml</loc>
   </sitemap>
 </sitemapindex>
 EndSitemapIndex
