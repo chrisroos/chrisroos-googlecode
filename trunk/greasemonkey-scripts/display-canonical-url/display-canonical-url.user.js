7348ef1f51351529d5691b30364eb9ee9b505343
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-07-21 08:39:39 +0000

    Update the description to point to the script that I've rewritten here.  Change the type of comments so that they play nicely with firebug when it removes all the newlines (// causes problems, where /* and */ don't).

--- trunk/greasemonkey-scripts/display-canonical-url/display-canonical-url.user.js
+++ trunk/greasemonkey-scripts/display-canonical-url/display-canonical-url.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Display Canonical Urls
 // @namespace     http://www.chrisroos.co.uk
-// @description   Scans the head of the document for the first link element with a rel=canonical attribute/value and displays the value of the href attribute (the canonical url) for that element.  It will additionally link to the canonical url if it's different to the current location.
+// @description   Scans the head of the document for the first link element with a rel=canonical attribute/value and displays the value of the href attribute (the canonical url) for that element.  It will additionally link to the canonical url if it's different to the current location.  This is a rewrite of the show-canonical script linked to from this post (http://www.tripwolf.com/en/blog/2009/03/17/offtopic-canonical-a-powerful-seo-concept/).
 // @include       *
 // ==/UserScript==
 
@@ -12,15 +12,15 @@
   
   for (var i = 0; i < linkElements.length; i++) {
     var linkElement = linkElements[i];
-    // Use a case-insensitive regex to deal with differences in the way people write 'canonical'.
+    /* Use a case-insensitive regex to deal with differences in the way people write 'canonical'. */
     if (/canonical/i.test(linkElement.rel)) { 
       canonicalLinkElements.push(linkElement)
     }
   }
   
   if (canonicalLinkElements.length > 0) {
-    // Use elm.href (instead of elm.getAttribute('href')) because it automatically deals with <base /> elements and absolute and relative values of the link's href attribute.
-    // Although there could potentially be more than one link rel=canonical in the page I'm intentionally only taking the first one.
+    /* Use elm.href (instead of elm.getAttribute('href')) because it automatically deals with <base /> elements and absolute and relative values of the link's href attribute. */
+    /* Although there could potentially be more than one link rel=canonical in the page I'm intentionally only taking the first one. */
     var canonicalUrl = canonicalLinkElements[0].href;
     
     var canonicalUrlContainer            = document.createElement('div');
@@ -33,7 +33,7 @@
     canonicalUrlContainer.style.border   = '1px solid';
     
     if (canonicalUrl != document.location) {
-      // Link to the Canonical Url as it's different to our current location
+      /* Link to the Canonical Url as it's different to our current location */
       canonicalUrlContainer.style.backgroundColor = 'yellow';
       canonicalUrlContainer.style.borderColor     = 'red';
       canonicalUrlContainer.style.color           = 'blue';
@@ -43,7 +43,7 @@
       anchor.appendChild(document.createTextNode(canonicalUrl));
       canonicalUrlContainer.appendChild(anchor);
     } else {
-      // Display the Canonical Url as it's the same as our current location
+      /* Display the Canonical Url as it's the same as our current location */
       canonicalUrlContainer.style.backgroundColor = 'lightyellow';
       canonicalUrlContainer.style.borderColor     = 'gray';
       canonicalUrlContainer.style.color           = 'black';
