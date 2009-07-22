cca1130e290da2cd1210be02f173d31794645fd3
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-07-22 13:39:37 +0000

    This change fixes the problem whereby multiple handlers were being set-up for the click event of the widget.  Although I was unbinding the event if the user navigated to a page without a canonical url, additional click events would be registered if the user navigated to a page that did have a canonical url (or if they just refreshed the page they were on that had a canonical url).  The solution was to extract the 'finding of the canonical url' functionality into a function and reuse that in the 'highlighting/ghosting of the icon' and the 'notifying of the canonical url itself'.

--- trunk/jetpack-features/show-canonical.jetpack.js
+++ trunk/jetpack-features/show-canonical.jetpack.js
@@ -10,30 +10,38 @@ jetpack.statusBar.append({
     // Keep a reference to the statusbar widget (html document)
     canonicalUrlIndicatorDocument = widget;
     // Call our function when a tab gets focus and when its reloaded
-    jetpack.tabs.onReady(showCanonicalUrl);
-    jetpack.tabs.onFocus(showCanonicalUrl);
+    jetpack.tabs.onReady(highlightWidget);
+    jetpack.tabs.onFocus(highlightWidget);
+    // Add the click event to the widget that displays the canonical url if it exists
+    $(widget).click(displayCanonicalUrl);
   }
 });
 
-var showCanonicalUrl = function() {
+var findCanonicalUrl = function() {
   // Get the html content of the document in the current tab
   var doc = jetpack.tabs.focused.contentDocument;
   // Pick out the first link rel=canonical element
-  var canonicalLink = $("link[rel='canonical']", doc)[0];
+  return $("link[rel='canonical']", doc)[0];
+}
+
+var highlightWidget = function() {
   var canonicalUrlIndicatorWidget = $('#canonicalUrlIndicator', canonicalUrlIndicatorDocument);
+  var canonicalLink = findCanonicalUrl();
   if (canonicalLink) {
     // Make the permalink icon 'active'
     canonicalUrlIndicatorWidget.css('color', '#ff0');
-    // Pretty useless at the moment but display a notification if the user clicks on the 'active' permalink icon
-    canonicalUrlIndicatorWidget.click(function() {
-      jetpack.notifications.show({
-        title : 'Canonical Url',
-        body : canonicalLink.href
-      }); 
-    });
   } else {                       
-    // If we didn't find a permalink in the page then let's 'ghost' the permalink icon and unregister the click event handler
+    // If we didn't find a permalink in the page then let's 'ghost' the permalink icon
     canonicalUrlIndicatorWidget.css('color', '#e1e1e1');
-    canonicalUrlIndicatorWidget.unbind('click');
+  }
+}
+
+var displayCanonicalUrl = function() {
+  var canonicalLink = findCanonicalUrl();
+  if (canonicalLink) {
+    jetpack.notifications.show({
+      title : 'Canonical Url',
+      body : canonicalLink.href
+    });
   }
 }
