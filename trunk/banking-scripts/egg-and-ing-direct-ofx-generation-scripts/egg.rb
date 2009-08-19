aeafdb14d1d0da39c822970650e0d17921ff6572
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-08-19 13:09:38 +0000

    The location of the statement date range (x to y) has changed (we now need to read it from the drop down list) and I've defaulted to OFX 2.

--- trunk/banking-scripts/egg-and-ing-direct-ofx-generation-scripts/egg.rb
+++ trunk/banking-scripts/egg-and-ing-direct-ofx-generation-scripts/egg.rb
@@ -1,7 +1,9 @@
+require 'rubygems'
+
 require File.dirname(__FILE__) + '/lib/egg'
 require File.dirname(__FILE__) + '/lib/ofx'
 
-require 'rubygems'
+
 require 'hpricot'
 
 StatementsFolder = File.dirname(__FILE__) + '/statements/'
@@ -11,11 +13,12 @@ Dir[StatementsFolder + '*.html'].each do |statement_html_file|
   
   html = File.open(statement_html_file) { |f| f.read }
   doc = Hpricot(html)
-  
+
   card_number = (doc/"span#lblCardNumber").inner_html
-  statement_date = (doc/"span#lblStatementDate").inner_html
+  statement_date = (doc/"#ctl00_content_eggCardStatements_lstPreviousStatements option[@selected=selected]").inner_text
   closing_balance = ((doc/"table#tblTransactionsTable"/"tfoot"/"tr").first/"td").inner_html
   account = Egg::Account.new('GBP', card_number)
+  
   statement = Egg::Statement.new(statement_date, closing_balance, account)
   
   (doc/"table#tblTransactionsTable"/"tbody"/"tr").each do |row|
@@ -35,15 +38,15 @@ Dir[StatementsFolder + '*.html'].each do |statement_html_file|
   filename = File.basename(statement_html_file, '.html')
   File.open(OfxOutputFolder + filename + '.ofx', 'w') do |file|
     # OFX 1.0 format - SGML
-    file.puts 'OFXHEADER:100'
-    file.puts 'VERSION:103'
-    file.puts 'SECURITY:NONE'
-    file.puts 'ENCODING:UNICODE'
-    file.puts 'OLDFILEUID:NONE'
-    file.puts 'NEWFILEUID:NONE'
+    # file.puts 'OFXHEADER:100'
+    # file.puts 'VERSION:103'
+    # file.puts 'SECURITY:NONE'
+    # file.puts 'ENCODING:UNICODE'
+    # file.puts 'OLDFILEUID:NONE'
+    # file.puts 'NEWFILEUID:NONE'
     # OFX 2.0 format - XML
-    # file.puts %q[<?xml version="1.0" encoding="UTF-8"?>]
-    # file.puts %q[<?OFX OFXHEADER="200" VERSION="200" SECURITY="NONE" OLDFILEUID="NONE" NEWFILEUID="NONE"?>]
+    file.puts %q[<?xml version="1.0" encoding="UTF-8"?>]
+    file.puts %q[<?OFX OFXHEADER="200" VERSION="200" SECURITY="NONE" OLDFILEUID="NONE" NEWFILEUID="NONE"?>]
     file.puts(ofx_s.to_xml)
   end
   
