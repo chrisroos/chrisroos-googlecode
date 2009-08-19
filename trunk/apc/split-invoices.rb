97fd72caf7a75ade3e039901cafd4c220bebfd3d
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-08-19 06:49:02 +0000

    Made the script a little more usable by reading from STDIN and added some usage comments.

--- trunk/apc/split-invoices.rb
+++ trunk/apc/split-invoices.rb
@@ -1,11 +1,30 @@
-invoices = File.read('APC-INVOICES.txt')
-# invoices = File.read('multipage-invoice.txt')
+invoices_file = ARGV.shift
+unless invoices_file && File.exists?(invoices_file)
+  puts ""
+  puts "Split Invoices"
+  puts "--------------"
+  puts "Usage: split-invoices.rb all-invoices-file [output-directory]"
+  puts ""
+  puts "  * all-invoices-file (required): Path to the file containing all invoices exported from APC Depot software"
+  puts "  * output-directory (optional): Directory to write the individual invoices to (defaults to same directory as all-invoices file)"
+  puts ""
+  exit
+end
+
+unless output_directory = ARGV.shift
+  output_directory = File.dirname(invoices_file)
+end
+
+# Read all the invoices into memory
+invoices = File.read(invoices_file)
 
 invoice = ''
 invoices.each do |line|
-  if line =~ /^\f/ and invoice =~ /PLEASE PAY BY/ # Form feed, page eject
+  # Form feed/page eject (\f) represents end of page (as this format is designed to be printed)
+  # /PLEASE PAY BY/ ensures that we deal with multi-page invoices (rather than blindly splitting on form feed)
+  if line =~ /^\f/ and invoice =~ /PLEASE PAY BY/
     account_number = invoice[/ACCOUNT: (.*)$/, 1].chomp
-    File.open(File.join('invoices', "#{account_number}.txt"), 'w') { |f| f.puts(invoice) }
+    File.open(File.join(output_directory, "#{account_number}.txt"), 'w') { |f| f.puts(invoice) }
     invoice = ''
   else
     invoice << line
