d0ef59723c7a4a3548e79bdcd94761d9476eef18
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-09-08 22:01:27 +0000

    I've had to rip the procedural code out of egg.rb and into a very crappy object just so that I can re-use it to make my new integration tests pass.

--- trunk/banking-scripts/egg-and-ing-direct-ofx-generation-scripts/egg.rb
+++ trunk/banking-scripts/egg-and-ing-direct-ofx-generation-scripts/egg.rb
@@ -11,34 +11,11 @@ OfxOutputFolder = File.dirname(__FILE__) + '/ofx/'
 Dir[StatementsFolder + '*.html'].each do |statement_html_file|
   
   html = File.open(statement_html_file) { |f| f.read }
-  doc = Hpricot(html)
-
-  card_number = (doc/"span#lblCardNumber").inner_html
-  statement_date = (doc/'#ctl00_content_eggCardStatements_lstPreviousStatements'/'option[@selected=selected]').first.attributes['value']
-  closing_balance = ((doc/"table#tblTransactionsTable"/"tfoot"/"tr").first/"td").inner_html
-  account = Egg::Account.new('GBP', card_number)
-  
-  statement = Egg::Statement.new(statement_date, closing_balance, account)
-  
-  (doc/"table#tblTransactionsTable"/"tbody"/"tr").each do |row|
-    date = (row/"td.date").inner_text
-    description = (row/"td.description").inner_text
-    money = (row/"td.money").inner_text
-    next if date == '' || description == 'OPENING BALANCE'
-    if row.next_sibling && (row.next_sibling/"td.date").inner_text == '' && (row.next_sibling/"td.description").inner_text != ''
-      description += ' / ' + (row.next_sibling/"td.description").inner_text
-    end
-    transaction = Egg::Transaction.new(date, description, money)
-    statement.add_transaction(transaction)
-  end
-  
-  ofx_s = Ofx::Statement.new(statement)
+  statement_parser = Egg::StatementParser.new(html)
   
   filename = File.basename(statement_html_file, '.html')
   File.open(OfxOutputFolder + filename + '.ofx', 'w') do |file|
-    file.puts %q[<?xml version="1.0" encoding="UTF-8"?>]
-    file.puts %q[<?OFX OFXHEADER="200" VERSION="200" SECURITY="NONE" OLDFILEUID="NONE" NEWFILEUID="NONE"?>]
-    file.puts(ofx_s.to_xml)
+    file.puts statement_parser.to_ofx
   end
   
 end
