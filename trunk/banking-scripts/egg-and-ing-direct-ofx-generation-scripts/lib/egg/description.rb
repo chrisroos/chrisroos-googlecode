70f38304587ca90a47b98008e07570b5ffa65555
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-11-07 16:30:59 +0000

    Added a parser for the recent transactions html that's slightly different to the standard statement html. NOTE. I had to change the Description object as the two character territory only appears in the statement html and not in the recent transaction html.

--- trunk/banking-scripts/egg-and-ing-direct-ofx-generation-scripts/lib/egg/description.rb
+++ trunk/banking-scripts/egg-and-ing-direct-ofx-generation-scripts/lib/egg/description.rb
@@ -2,10 +2,10 @@ module Egg
   class Description
     attr_reader :payee, :note, :territory
     def initialize(description)
-      if m = description.match(/(.{23}) (.{13}) (.{2})/)
+      if m = description.match(/(.{23}) (.{13}) (.{2})?/) # territory (last two chars) is not in recent transactions
         @payee = m[1].strip.squeeze(' ')
         @note = m[2].strip
-        @territory = m[3].strip
+        @territory = m[3].strip if m[3]
       else
         @payee = description
       end
