564b70b456b8051c72aa116e9778a442603cf87a
Chris Roos <chris@seagul.co.uk>
2009-11-10 09:23:38 +0000

    This little binary interface to the parser is more useful (I think) than the 'put files in this specially named folder and get the output in this other specially name folder' previous version.

--- banking-scripts/egg-and-ing-direct-ofx-generation-scripts/egg.rb
+++ banking-scripts/egg-and-ing-direct-ofx-generation-scripts/egg.rb
@@ -1,16 +1,12 @@
+#!/usr/bin/env ruby
 require File.dirname(__FILE__) + '/lib/egg'
 
-StatementsFolder = File.dirname(__FILE__) + '/statements/'
-OfxOutputFolder = File.dirname(__FILE__) + '/ofx/'
-
-Dir[StatementsFolder + '*.html'].each do |statement_html_file|
-  
-  html = File.open(statement_html_file) { |f| f.read }
-  parser = Egg::Parser.new(html)
-  
-  filename = File.basename(statement_html_file, '.html')
-  File.open(OfxOutputFolder + filename + '.ofx', 'w') do |file|
-    file.puts parser.to_ofx
-  end
-  
+html_file = ARGV.shift
+unless html_file and File.exists?(html_file)
+  raise "Usage: egg2ofx path/to/egg-html-file"
 end
+
+html   = File.open(html_file) { |f| f.read }
+parser = Egg::Parser.new(html)
+
+puts parser.to_ofx
