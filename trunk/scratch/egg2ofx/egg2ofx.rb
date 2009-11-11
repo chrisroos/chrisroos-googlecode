957cd2d3c43ab9b394402b160b89c1730a94b8e4
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-11-11 09:16:11 +0000

    Add a sensible error message if we can't parse the html.

--- trunk/scratch/egg2ofx/egg2ofx.rb
+++ trunk/scratch/egg2ofx/egg2ofx.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'sinatra'
-require '/Users/chrisroos/Code/personal/chrisroos/banking-scripts/egg-and-ing-direct-ofx-generation-scripts/lib/egg.rb'
+require '/Users/chrisroos/Code/personal/chrisroos/banking-scripts/egg2ofx/lib/egg.rb'
+
+# TODO - rename this to egg2ofx-server
 
 # TODO - work out a nice way of including the egg library.  i could vendorise it or create a gem from it and use gembundler or summit
 #      - REMEMBER TO DO THE SIMPLEST THING TO START WITH - probably just svn:externals
@@ -10,11 +12,17 @@ get '/' do
   "<h1>egg2ofx</h1>" # TODO - add a nice little introduction page thing
 end
 
+# TODO - add a manual upload page
+
 post '/' do  
   content_type 'text/ofx' # TODO - what is the correct value here?
   attachment 'statement.ofx'
 
   html = params['documentHtml']
-  parser = Egg::Parser.new(html)
-  parser.to_ofx
+  begin
+    parser = Egg::Parser.new(html)
+    parser.to_ofx
+  rescue Egg::Parser::UnparsableHtmlError
+    error "Well, that didn't work, did it."
+  end
 end
