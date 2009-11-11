825e52940d9f05e733fc933d7fea6e111fa7d4ce
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-11-11 09:20:13 +0000

    Add a very simple test that we're on an egg statement or recent transactions page before going ahead and creating all secret little form.

--- trunk/greasemonkey-scripts/egg2ofx/egg2ofx.user.js
+++ trunk/greasemonkey-scripts/egg2ofx/egg2ofx.user.js
@@ -1,38 +1,43 @@
 // ==UserScript==
 // @name          Egg to Ofx
 // @namespace     http://chrisroos.co.uk/
-// @description   Adds a button to the Egg statement page that allows you to convert the statement to Ofx.
+// @description   Adds a button to the Egg statement and recent transactions pages that allows you to download them as Ofx.
 // @include       *egg.local*
 // @include       *egg.com*
 // ==/UserScript==
 
 (function() {
   try {
-    var f = document.createElement('form');
-    f.setAttribute('id', 'convertToOfxForm');
-    f.setAttribute('method', 'POST');
-    f.setAttribute('action', 'http://egg2ofx.local/');
     
-    var b = document.createElement('input');
-    b.setAttribute('type', 'button');
-    b.setAttribute('value', 'Download as OFX');
-    var convertToOfxFormSubmitFunction = function() { 
-      var h = document.createElement('input');
-      h.setAttribute('type', 'hidden');
-      h.setAttribute('name', 'documentHtml');
-      h.setAttribute('value', document.body.innerHTML);
-      var f = document.getElementById('convertToOfxForm')
-      f.appendChild(h);
-      f.submit();
+    var egg2ofxService = 'http://egg2ofx.local/';
+    var transactionsTable;
+    
+    if (transactionsTable = document.getElementById('tblTransactionsTable')) {
+      var f = document.createElement('form');
+      f.setAttribute('id', 'convertToOfxForm');
+      f.setAttribute('method', 'POST');
+      f.setAttribute('action', egg2ofxService);
+    
+      var b = document.createElement('input');
+      b.setAttribute('type', 'button');
+      b.setAttribute('value', 'Download as OFX');
+      var convertToOfxFormSubmitFunction = function() { 
+        var h = document.createElement('input');
+        h.setAttribute('type', 'hidden');
+        h.setAttribute('name', 'documentHtml');
+        h.setAttribute('value', document.body.innerHTML);
+        var f = document.getElementById('convertToOfxForm')
+        f.appendChild(h);
+        f.submit();
+      }
+      b.addEventListener('click', convertToOfxFormSubmitFunction, true);
+    
+      f.appendChild(b);
+      
+      transactionsTable.parentNode.insertBefore(f, transactionsTable);
     }
-    b.addEventListener('click', convertToOfxFormSubmitFunction, true);
-    
-    f.appendChild(b);
-  
-    var transactionsTable = document.getElementById('tblTransactionsTable');
-    transactionsTable.parentNode.insertBefore(f, transactionsTable);
   } catch(e) {
-    console.log('error');
+    console.log('egg2ofx.user.js error');
     console.log(e);
   }
 })()
