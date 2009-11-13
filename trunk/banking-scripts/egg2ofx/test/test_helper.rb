28ceb05426ec60dee527a29bf8f60dfa53ade212
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-11-13 20:52:14 +0000

    Removing the 'already initialized constant FIXTURES' errors.

--- trunk/banking-scripts/egg2ofx/test/test_helper.rb
+++ trunk/banking-scripts/egg2ofx/test/test_helper.rb
@@ -4,4 +4,4 @@ require 'mocha'
 
 require File.join(File.dirname(__FILE__), '..', 'lib', 'egg')
 
-FIXTURES = File.join(File.dirname(__FILE__), 'fixtures')
+FIXTURES = File.join(File.dirname(__FILE__), 'fixtures') unless defined?(FIXTURES)
