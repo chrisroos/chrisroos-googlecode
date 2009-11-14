d9cfce2cb31011e8805a6198e2e053ad303c02d1
Chris Roos <chris@seagul.co.uk>
2009-11-14 12:30:07 +0000

    Overriding the default deploy stop/start/restart tasks to cater for us using passenger.

--- scratch/egg2ofx-server/config/deploy.rb
+++ scratch/egg2ofx-server/config/deploy.rb
@@ -4,3 +4,20 @@ set :repository,  "http://chrisroos.googlecode.com/svn/trunk/scratch/#{applicati
 role :app, "seagul.co.uk"
 role :web, "seagul.co.uk"
 role :db,  "seagul.co.uk", :primary => true
+
+# after 'deploy:update_code', 'bundler:rebundle'
+
+namespace :deploy do
+  task :start do; end
+  task :stop do; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
+end
+
+# namespace :bundler do
+#   desc "run gem bundler"
+#   task :rebundle do
+#     run "cd #{release_path} && gem bundle --cached"
+#   end
+# end
