6c7cf5b0b3b932f9792398567be67232a12441c5
chrisjroos <chrisjroos@0b96500f-0b1b-0410-995a-f5c301e31718>
2009-11-14 12:55:56 +0000

    Reminding myself about what I need to do to get the egg2ofx server deployed.

--- trunk/scratch/egg2ofx-server/config/deploy.rb
+++ trunk/scratch/egg2ofx-server/config/deploy.rb
@@ -5,7 +5,7 @@ role :app, "seagul.co.uk"
 role :web, "seagul.co.uk"
 role :db,  "seagul.co.uk", :primary => true
 
-# after 'deploy:update_code', 'bundler:rebundle'
+after 'deploy:update_code', 'bundler:rebundle'
 
 namespace :deploy do
   task :start do; end
@@ -15,9 +15,12 @@ namespace :deploy do
   end
 end
 
-# namespace :bundler do
-#   desc "run gem bundler"
-#   task :rebundle do
-#     run "cd #{release_path} && gem bundle --cached"
-#   end
-# end
+namespace :bundler do
+  desc "run gem bundler"
+  task :rebundle do
+    # run "cd #{release_path} && gem bundle --cached"
+    puts "I haven't got to work automatically yet.  For now, change into the new release directory and run:"
+    puts "$ sudo gem bundle"
+    puts "We need to use sudo because otherwise hpricot doesn't install."
+  end
+end
