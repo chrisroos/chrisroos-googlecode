2203b0594e3225805a9568bde82a9fb3c8890228
Chris Roos <chris@seagul.co.uk>
2010-10-08 17:19:15 +0000

    This has now moved to github

--- websites/chrisroos.co.uk/lib/blog_helper.rb
+++
@@ -1,8 +0,0 @@
-module BlogHelper
-  def blog_posts(number = :all, options = {}, &blk)
-    defaults = {:in_directory => "blog", :sort_by => "created_at", :reverse => true, :blog_post => true}
-    @pages.find(number, defaults.merge(options), &blk)
-  end
-end
-
-Webby::Helpers.register(BlogHelper)
